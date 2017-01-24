d4697e18fa237b27bd14fbffae45688e23aa3268
thoradia <thoradia.quack@gmail.com>
2017-01-24 23:49:52 +0100

    boost: rework

--- packages/devel/boost/package.mk
+++ packages/devel/boost/package.mk
@@ -25,7 +25,7 @@ PKG_SITE="http://www.boost.org/"
 PKG_URL="$SOURCEFORGE_SRC/boost/boost/1.61.0/${PKG_NAME}_${PKG_VERSION}.tar.bz2"
 PKG_SOURCE_DIR="${PKG_NAME}_${PKG_VERSION}"
 PKG_DEPENDS_HOST=""
-PKG_DEPENDS_TARGET="toolchain boost:host Python:host zlib bzip2"
+PKG_DEPENDS_TARGET="toolchain boost:host Python zlib bzip2"
 PKG_SECTION="devel"
 PKG_SHORTDESC="boost: Peer-reviewed STL style libraries for C++"
 PKG_LONGDESC="Boost provides free peer-reviewed portable C++ source libraries. The emphasis is on libraries which work well with the C++ Standard Library. One goal is to establish existing practice and provide reference implementations so that the Boost libraries are suitable for eventual standardization. Some of the libraries have already been proposed for inclusion in the C++ Standards Committee's upcoming C++ Standard Library Technical Report."
@@ -53,6 +53,7 @@ configure_target() {
   sh bootstrap.sh --prefix=/usr \
                   --with-bjam=$TOOLCHAIN/bin/bjam \
                   --with-python=$TOOLCHAIN/bin/python \
+                  --with-python-root=$SYSROOT_PREFIX/usr
 
   echo "using gcc : `$CC -v 2>&1  | tail -n 1 |awk '{print $3}'` : $CC  : <compileflags>\"$CFLAGS\" <linkflags>\"$LDFLAGS\" ;" \
     > tools/build/src/user-config.jam
@@ -73,5 +74,14 @@ makeinstall_target() {
                                 --with-serialization \
                                 --with-filesystem \
                                 --with-regex -sICU_PATH="$SYSROOT_PREFIX/usr" \
+                                --with-chrono \
+                                --with-date_time \
+                                --with-random \
+                                install
+  $TOOLCHAIN/bin/bjam -d2 --toolset=gcc link=shared \
+                                --prefix=$SYSROOT_PREFIX/usr \
+                                --ignore-site-config \
+                                --layout=system \
+                                --with-python \
                                 install
 }
