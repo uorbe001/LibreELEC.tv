4374138d898fb649502083f0f6442ca83fb2d027
thoradia <thoradia.quack@gmail.com>
2017-01-25 00:03:20 +0100

    libtorrent-rasterbar: rework

---
+++ packages/thoradia/depends/libtorrent-rasterbar/package.mk
@@ -0,0 +1,23 @@
+PKG_NAME="libtorrent-rasterbar"
+PKG_VERSION="1_0_10"
+PKG_LICENSE="https://github.com/arvidn/libtorrent/blob/master/LICENSE"
+PKG_SITE="http://libtorrent.org/"
+PKG_URL="https://github.com/arvidn/libtorrent/archive/libtorrent-$PKG_VERSION.tar.gz"
+PKG_SOURCE_DIR="libtorrent-libtorrent-$PKG_VERSION"
+PKG_DEPENDS_TARGET="toolchain boost libiconv libressl"
+PKG_LONGDESC="An efficient feature complete C++ bittorrent implementation"
+PKG_USE_CMAKE="no"
+PKG_AUTORECONF="yes"
+
+PKG_CONFIGURE_OPTS_TARGET="--enable-python-binding \
+                           --with-boost-libdir=$SYSROOT_PREFIX/usr/lib \
+                           --with-libiconv"
+
+post_unpack() {
+  mkdir -p $ROOT/$PKG_BUILD/build-aux/
+  touch $ROOT/$PKG_BUILD/build-aux/config.rpath
+}
+
+pre_configure_target() {
+  cp -PR ../* .
+}
