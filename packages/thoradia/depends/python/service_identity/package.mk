55dc02c4322e5b5d0b59f83a1ba8768d08ca30fd
thoradia <thoradia.quack@gmail.com>
2017-01-25 00:16:52 +0100

    service_indentity: rework

---
+++ packages/thoradia/depends/python/service_identity/package.mk
@@ -0,0 +1,20 @@
+PKG_NAME="service_identity"
+PKG_VERSION="16.0.0"
+PKG_LICENSE="MIT"
+PKG_SITE="https://service-identity.readthedocs.io/en/stable/"
+PKG_URL="https://github.com/pyca/$PKG_NAME/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain distutilscross:host Python"
+PKG_LONGDESC="Service identity verification for pyOpenSSL"
+
+make_target() {
+  export LDSHARED="$CC -shared"
+  export PYTHONXCPREFIX="$SYSROOT_PREFIX/usr"
+  python setup.py build --cross-compile
+}
+
+makeinstall_target() {
+  python setup.py install --root=$INSTALL --prefix=/usr
+  find $INSTALL/usr/lib -name "*.py" -exec rm -rf "{}" ";"
+  rm -rf $INSTALL/usr/lib/python*/site-packages/*.egg-info \
+         $INSTALL/usr/lib/python*/site-packages/*/tests
+}
