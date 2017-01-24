7821e3a94633a82db8f7d9e8136ec224aaa9452f
thoradia <thoradia.quack@gmail.com>
2017-01-25 00:06:21 +0100

    croptography: rework

---
+++ packages/thoradia/depends/python/cryptography/package.mk
@@ -0,0 +1,20 @@
+PKG_NAME="cryptography"
+PKG_VERSION="1.7.1"
+PKG_LICENSE="BSD"
+PKG_SITE="https://cryptography.io/"
+PKG_URL="https://github.com/pyca/$PKG_NAME/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain cffi:host Python"
+PKG_LONGDESC="A package designed to expose cryptographic primitives and recipes to Python developers"
+
+make_target() {
+  export LDSHARED="$CC -shared"
+  export PYTHONXCPREFIX="$SYSROOT_PREFIX/usr"
+  python setup.py build --cross-compile
+}
+
+makeinstall_target() {
+  python setup.py install --root=$INSTALL --prefix=/usr
+  find $INSTALL/usr/lib -name "*.py" -exec rm -rf "{}" ";"
+  rm -rf $INSTALL/usr/lib/python*/site-packages/*.egg-info \
+         $INSTALL/usr/lib/python*/site-packages/*/tests
+}
