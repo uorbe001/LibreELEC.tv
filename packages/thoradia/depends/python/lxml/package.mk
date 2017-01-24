a00cafb8bea6964ae7288feffe2be0ed72fa0aa2
thoradia <thoradia.quack@gmail.com>
2017-01-25 00:14:32 +0100

    lxml: rework

---
+++ packages/thoradia/depends/python/lxml/package.mk
@@ -0,0 +1,21 @@
+PKG_NAME="lxml"
+PKG_VERSION="3.7.1"
+PKG_LICENSE="BSD"
+PKG_SITE="http://lxml.de/"
+PKG_URL="https://github.com/lxml/lxml/archive/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_SOURCE_DIR="lxml-$PKG_NAME-$PKG_VERSION"
+PKG_DEPENDS_TARGET="toolchain cython:host libxslt:host Python libxml2 libxslt"
+PKG_LONGDESC="The lxml XML toolkit for Python"
+
+make_target() {
+  export LDSHARED="$CC -shared"
+  export PYTHONXCPREFIX="$SYSROOT_PREFIX/usr"
+  python setup.py build --cross-compile
+}
+
+makeinstall_target() {
+  python setup.py install --root=$INSTALL --prefix=/usr
+  find $INSTALL/usr/lib -name "*.py" -exec rm -rf "{}" ";"
+  rm -rf $INSTALL/usr/lib/python*/site-packages/*.egg-info \
+         $INSTALL/usr/lib/python*/site-packages/*/tests
+}
