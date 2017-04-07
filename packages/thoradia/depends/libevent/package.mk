9e3be61e10565c78b28e97d948505b4074a07f3b
thoradia <thoradia.quack@gmail.com>
2017-04-07 08:46:33 +0200

    libevent: update to 2.1.8-stable

--- packages/thoradia/depends/libevent/package.mk
+++ packages/thoradia/depends/libevent/package.mk
@@ -1,12 +1,12 @@
 PKG_NAME="libevent"
-PKG_VERSION="2.0.22-stable"
+PKG_VERSION="2.1.8-stable"
 PKG_LICENSE="BSD"
 PKG_SITE="http://libevent.org/"
 PKG_URL="https://github.com/libevent/libevent/releases/download/release-$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain libressl zlib"
 PKG_SHORTDESC="libevent: a library for asynchronous event notification"
 
-PKG_CONFIGURE_OPTS_TARGET="--disable-shared --enable-static --enable-openssl"
+PKG_CONFIGURE_OPTS_TARGET="--disable-libevent-regress --disable-samples --disable-shared --enable-static --enable-openssl"
 
 post_makeinstall_target() {
   rm -fr $INSTALL
