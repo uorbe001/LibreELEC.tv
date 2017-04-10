288aa94005ead745496526e8c368366eac8a415c
thoradia <thoradia.quack@gmail.com>
2017-04-10 09:03:03 +0200

    sabnzbd: update to 2.0.0

--- packages/thoradia/addons/sabnzbd/package.mk
+++ packages/thoradia/addons/sabnzbd/package.mk
@@ -1,16 +1,16 @@
 PKG_NAME="sabnzbd"
-PKG_VERSION="1.2.3"
+PKG_VERSION="2.0.0"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://sabnzbd.org/"
 PKG_URL="https://github.com/sabnzbd/$PKG_NAME/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain setuptools:host cheetah cffi cryptography enum34 par2cmdline p7zip py-notify unrar yenc"
+PKG_DEPENDS_TARGET="toolchain setuptools:host cheetah cffi cryptography enum34 par2cmdline p7zip py-notify sabyenc unrar"
 PKG_SECTION="service"
 
 PKG_IS_ADDON="yes"
 PKG_ADDON_NAME="SABnzbd"
 PKG_ADDON_TYPE="xbmc.service"
 PKG_MAINTAINER="thoradia"
-PKG_REV="12"
+PKG_REV="13"
 PKG_SHORTDESC="$PKG_ADDON_NAME: free and easy binary newsreader"
 PKG_LONGDESC="$PKG_ADDON_NAME ($PKG_VERSION) is a program to download binary files from Usenet servers."
 PKG_DISCLAIMER="Keep it legal and carry on"
@@ -36,7 +36,7 @@ addon() {
          $(get_build_dir setuptools)/build/lib/pkg_resources \
          $ADDON_BUILD/$PKG_ADDON_ID/SABnzbd/
 
-  for d in cheetah cffi cryptography enum34 packaging py-notify yenc; do
+  for d in cheetah cffi cryptography enum34 packaging py-notify sabyenc; do
     cp -PR $(get_build_dir $d)/.install_pkg/usr/lib/python*/site-packages/* \
            $ADDON_BUILD/$PKG_ADDON_ID/SABnzbd/
   done
