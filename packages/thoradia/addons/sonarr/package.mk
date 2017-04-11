6b52e7a19fa0b78ad0fab408277fd2754ecd23a8
thoradia <thoradia.quack@gmail.com>
2017-04-11 10:35:18 +0200

    sonarr: move thoradia-libmediainfo to virtual.thoradia-mono

--- packages/thoradia/addons/sonarr/package.mk
+++ packages/thoradia/addons/sonarr/package.mk
@@ -3,15 +3,15 @@ PKG_VERSION="2.0.0.4689"
 PKG_LICENSE="GPL3"
 PKG_SITE="https://sonarr.tv/"
 PKG_URL="http://download.sonarr.tv/v2/master/mono/NzbDrone.master.$PKG_VERSION.mono.tar.gz"
-PKG_DEPENDS_TARGET="toolchain thoradia_libmediainfo"
+PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="service"
 
 PKG_IS_ADDON="yes"
 PKG_ADDON_NAME="Sonarr"
 PKG_ADDON_TYPE="xbmc.service"
-PKG_ADDON_REQUIRES="tools.mono:0.0.0"
+PKG_ADDON_REQUIRES="virtual.thoradia-mono:0.0.0"
 PKG_MAINTAINER="thoradia"
-PKG_REV="14"
+PKG_REV="15"
 PKG_SHORTDESC="$PKG_ADDON_NAME: a PVR for Usenet and BitTorrent users"
 PKG_LONGDESC="$PKG_ADDON_NAME ($PKG_VERSION) is a PVR for Usenet and BitTorrent users. It can monitor multiple RSS feeds for new episodes of your favorite shows and will grab, sort and rename them. It can also be configured to automatically upgrade the quality of files already downloaded when a better quality format becomes available."
 PKG_DISCLAIMER="Keep it legal and carry on"
@@ -29,7 +29,6 @@ makeinstall_target() {
 }
 
 addon() {
-  mkdir -p "$ADDON_BUILD/$PKG_ADDON_ID/lib"
+  mkdir -p "$ADDON_BUILD/$PKG_ADDON_ID"
   tar xzf "$ROOT/$SOURCES/$PKG_NAME/$PKG_SOURCE_NAME" -C "$ADDON_BUILD/$PKG_ADDON_ID"
-  cp -L $(get_build_dir thoradia_libmediainfo)/.install_pkg/usr/lib/libmediainfo.so.0 $ADDON_BUILD/$PKG_ADDON_ID/lib/
 }
