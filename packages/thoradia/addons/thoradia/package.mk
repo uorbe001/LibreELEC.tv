dc22b0b559f08d9d63c5209abbf53c6156dbfd47
thoradia <thoradia.quack@gmail.com>
2017-05-24 01:06:52 +0200

    thoradia: mention qBittorrent

--- packages/thoradia/addons/thoradia/package.mk
+++ packages/thoradia/addons/thoradia/package.mk
@@ -7,9 +7,9 @@ PKG_IS_ADDON="yes"
 PKG_ADDON_NAME="Thoradia Add-ons"
 PKG_ADDON_TYPE="thoradia.service"
 PKG_MAINTAINER="thoradia"
-PKG_REV="5"
+PKG_REV="6"
 PKG_SHORTDESC="$PKG_ADDON_NAME: add-on repository"
-PKG_LONGDESC="$PKG_ADDON_NAME ($PKG_REV) add-on repository, provides CouchPotato, Deluge, Headphones, Jackett, NZBGet, Radarr, SABnzbd, SickRage, Sonarr and Transmission."
+PKG_LONGDESC="$PKG_ADDON_NAME ($PKG_REV) add-on repository, provides CouchPotato, Deluge, Headphones, Jackett, NZBGet, qBittorent, Radarr, SABnzbd, SickRage, Sonarr and Transmission."
 PKG_DISCLAIMER="Keep it legal and carry on"
 
 make_target() {
