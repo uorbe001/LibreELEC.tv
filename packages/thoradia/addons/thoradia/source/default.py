42206ef276c5811b092e552cf4d6e489c6528957
thoradia <thoradia.quack@gmail.com>
2017-06-01 11:39:19 +0200

    thoradia: rework default.py

--- packages/thoradia/addons/thoradia/source/default.py
+++ packages/thoradia/addons/thoradia/source/default.py
@@ -1,38 +1,56 @@
-from urlparse import urlparse
+import os.path
+import urllib2
 import xbmc
 import xbmcaddon
 import xbmcgui
 import xml.etree.ElementTree as etree
 
-REPO_LE = 'repository.libreelec.tv'
-REPO_TH = 'https://raw.githubusercontent.com/thoradia/thoradia/master/'
 
-try:
-  addon = xbmcaddon.Addon()
-  strings = addon.getLocalizedString
+URL_D = 'https://raw.githubusercontent.com/thoradia/thoradia/master/{}/{}/{}/'
+URL_L = 'https://github.com/thoradia/releases-libreelec/blob/master/{}-{}-{}'
+URL_U = 'https://github.com/thoradia/updates-thoradia/blob/master/{}-{}-{}'
 
-  xml_le = xbmcaddon.Addon(REPO_LE).getAddonInfo('path') + '/addon.xml'
-  path = urlparse(etree.parse(xml_le).iter(tag='datadir').next().text).path.strip('/').split('/')
-  vpa = path[-3] + '/' +  path[-2] + '/' +  path[-1] + '/'
-  new = REPO_TH + vpa
-  
-  xml_th = addon.getAddonInfo('path') + '/addon.xml'
-  xml = etree.parse(xml_th)
-  old = xml.iter(tag='datadir').next()
-  if old.text == new:
-    addon.setSetting('vpa', vpa)
-  else:
-    addon.setSetting('vpa', strings(30020))
-    old.text = new
-    xml.iter(tag='info').next().text = new + 'addons.xml'
-    xml.iter(tag='checksum').next().text = new + 'addons.xml.md5'
-    xml.write(xml_th)
-    if xbmcgui.Dialog().yesno(addon.getAddonInfo('name'),
-                              strings(30010),
-                              strings(30011),
-                              nolabel=strings(30012),
-                              yeslabel=strings(30013)) == False:
-      xbmc.executebuiltin('RestartApp')
 
-except:
-  addon.setSetting('vpa', strings(30021))
+def getTag(tree, tag):
+   return tree.iter(tag=tag).next()
+
+def getTree(xml):
+   return etree.parse(xml)
+
+def getXML(*id):
+   return os.path.join(xbmcaddon.Addon(*id).getAddonInfo('path'), 'addon.xml')
+
+
+if __name__ == '__main__':
+   addon = xbmcaddon.Addon()
+   strings = addon.getLocalizedString
+   addon.setSetting('le', strings(30010))
+   addon.setSetting('vpa', strings(30010))
+   release = getTag(getTree(getXML('repository.libreelec.tv')), 'datadir').text.strip('/').split('/')[-3:]
+   addon.setSetting('le', strings(30011).format(*release))
+   try:
+      urllib2.urlopen(URL_L.format(*release)).read()
+   except:
+      pass
+   # fix release here
+   xml = getXML()
+   tree = getTree(xml)
+   tag_d = getTag(tree, 'datadir')
+   url_d = URL_D.format(*release)
+   if tag_d.text == url_d:
+      addon.setSetting('vpa', strings(30011).format(*release))
+   else:
+      try:
+         urllib2.urlopen(URL_U.format(*release)).read()
+      except:
+         pass
+      tag_d.text = url_d
+      getTag(tree, 'info').text = url_d + 'addons.xml'
+      getTag(tree, 'checksum').text = url_d + 'addons.xml.md5'
+      tree.write(xml)
+      addon.setSetting('vpa', strings(30012).format(*release))
+      if xbmcgui.Dialog().yesno(addon.getAddonInfo('name'),
+                                strings(30013),
+                                nolabel=strings(30014),
+                                yeslabel=strings(30015)) == False:
+         xbmc.restart()
