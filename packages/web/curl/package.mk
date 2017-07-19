2546d982c68f11a514b6e46bf612655944581153
thoradia <thoradia.quack@gmail.com>
2017-07-19 22:38:58 +0200

    curl: update to 7.54.0 and build static

--- packages/web/curl/package.mk
+++ packages/web/curl/package.mk
@@ -40,7 +40,9 @@ PKG_IS_ADDON="no"
 PKG_USE_CMAKE="no"
 PKG_AUTORECONF="no"
 
-PKG_CONFIGURE_OPTS_TARGET="ac_cv_lib_rtmp_RTMP_Init=yes \
+PKG_CONFIGURE_OPTS_TARGET="--enable-static \
+                           --disable-shared \
+                           ac_cv_lib_rtmp_RTMP_Init=yes \
                            ac_cv_header_librtmp_rtmp_h=yes \
                            --disable-debug \
                            --enable-optimize \
