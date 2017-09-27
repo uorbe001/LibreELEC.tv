0eb84139992827525ec09a6e4610ab3a748108bb
thoradia <thoradia.quack@gmail.com>
2017-09-27 08:57:51 +0200

    six: update to 1.11.0

--- packages/thoradia/depends/python/six/package.mk
+++ packages/thoradia/depends/python/six/package.mk
@@ -1,9 +1,8 @@
 PKG_NAME="six"
-PKG_VERSION="1.10.0"
+PKG_VERSION="1.11.0"
 PKG_LICENSE="MIT"
 PKG_SITE="http://pypi.python.org/pypi/six/"
-PKG_URL="https://bitbucket.org/gutworth/$PKG_NAME/get/$PKG_VERSION.tar.gz"
-PKG_SOURCE_DIR="gutworth-$PKG_NAME-*"
+PKG_URL="https://github.com/benjaminp/$PKG_NAME/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain Python distutilscross:host"
 PKG_LONGDESC="Python 2 and 3 compatibility utilities"
 
@@ -16,6 +15,4 @@ make_target() {
 makeinstall_target() {
   python setup.py install --root=$INSTALL --prefix=/usr
   find $INSTALL/usr/lib -name "*.py" -exec rm -rf "{}" ";"
-  rm -rf $INSTALL/usr/lib/python*/site-packages/*.egg-info \
-         $INSTALL/usr/lib/python*/site-packages/*/tests
 }
