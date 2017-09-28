f53e6db5d77140b327ffbd388df1ceaf81a811a8
thoradia <thoradia.quack@gmail.com>
2017-09-28 07:48:08 +0200

    h2: initial package

---
+++ packages/thoradia/depends/python/h2/package.mk
@@ -0,0 +1,11 @@
+PKG_NAME="h2"
+PKG_VERSION="3.0.1"
+PKG_LICENSE="MIT"
+PKG_SITE="http://hyper.rtfd.org"
+PKG_URL="https://files.pythonhosted.org/packages/source/h/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain distutilscross:host Python"
+PKG_LONGDESC="HTTP/2 State-Machine based protocol implementation"
+
+PKG_IS_PYTHON="yes"
+PKG_PYTHON_DEPENDS_TARGET="enum34 hpack hyperframe"
+PKG_PYTHON_OPTS_TARGET="--cross-compile"
