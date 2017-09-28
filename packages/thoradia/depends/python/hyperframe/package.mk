a51fdbc11b336be9fbbfd9411ff78b58cef9d24e
thoradia <thoradia.quack@gmail.com>
2017-09-28 07:48:48 +0200

    hyperframe: initial package

---
+++ packages/thoradia/depends/python/hyperframe/package.mk
@@ -0,0 +1,10 @@
+PKG_NAME="hyperframe"
+PKG_VERSION="5.1.0"
+PKG_LICENSE="MIT"
+PKG_SITE="http://hyper.rtfd.org"
+PKG_URL="https://files.pythonhosted.org/packages/source/h/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain distutilscross:host Python"
+PKG_LONGDESC="HTTP/2 framing layer for Python"
+
+PKG_IS_PYTHON="yes"
+PKG_PYTHON_OPTS_TARGET="--cross-compile"
