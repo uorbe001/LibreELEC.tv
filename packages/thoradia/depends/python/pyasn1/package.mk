5ec2ab57d0ca066180cdff0414ae291858a51f30
thoradia <thoradia.quack@gmail.com>
2017-09-28 07:42:15 +0200

    pyasn1: python build

--- packages/thoradia/depends/python/pyasn1/package.mk
+++ packages/thoradia/depends/python/pyasn1/package.mk
@@ -6,15 +6,5 @@ PKG_URL="https://github.com/etingof/$PKG_NAME/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain distutilscross:host Python"
 PKG_LONGDESC="ASN.1 library for Python"
 
-make_target() {
-  export LDSHARED="$CC -shared"
-  export PYTHONXCPREFIX="$SYSROOT_PREFIX/usr"
-  python setup.py build --cross-compile
-}
-
-makeinstall_target() {
-  python setup.py install --root=$INSTALL --prefix=/usr
-  find $INSTALL/usr/lib -name "*.py" -exec rm -rf "{}" ";"
-  rm -rf $INSTALL/usr/lib/python*/site-packages/*.egg-info \
-         $INSTALL/usr/lib/python*/site-packages/*/tests
-}
+PKG_IS_PYTHON="yes"
+PKG_PYTHON_OPTS_TARGET="--cross-compile"
