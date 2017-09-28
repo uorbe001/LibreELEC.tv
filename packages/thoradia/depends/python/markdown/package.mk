f9e4a86cca824f375842d6e1484b7ad3bb909f94
thoradia <thoradia.quack@gmail.com>
2017-09-28 07:49:28 +0200

    markdown: initial package

---
+++ packages/thoradia/depends/python/markdown/package.mk
@@ -0,0 +1,9 @@
+PKG_NAME="markdown"
+PKG_VERSION="2.6.9"
+PKG_LICENSE="BSD"
+PKG_SITE="https://pythonhosted.org/Markdown"
+PKG_URL="https://github.com/Python-Markdown/$PKG_NAME/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain distutilscross:host Python"
+PKG_LONGDESC="A Python implementation of John Gruber’s Markdown with Extension support"
+
+PKG_IS_PYTHON="yes"
