e161b83da8c8176610a0a952c9188706876733b5
thoradia <thoradia.quack@gmail.com>
2017-09-28 07:28:24 +0200

    asn1crypto: python build

--- packages/thoradia/depends/python/asn1crypto/package.mk
+++ packages/thoradia/depends/python/asn1crypto/package.mk
@@ -6,15 +6,5 @@ PKG_URL="https://github.com/wbond/$PKG_NAME/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain distutilscross:host Python"
 PKG_LONGDESC="Python ASN.1 library with a focus on performance and a pythonic API"
 
-make_target() {
-  export LDSHARED="$CC -shared"
-  export PYTHONXCPREFIX="$SYSROOT_PREFIX/usr"
-  python setup.py build --cross-compile
-}
-
-makeinstall_target() {
-  python setup.py install --root=$INSTALL --prefix=/usr
-  find $INSTALL/usr/lib -name "*.py" -exec rm -rf "{}" ";"
-  rm -rf $INSTALL/usr/lib/python*/site-packages/*.egg-info \
-         $INSTALL/usr/lib/python*/site-packages/*/tests
-}
+PKG_IS_PYTHON="yes"
+PKG_PYTHON_OPTS_TARGET="--cross-compile"
