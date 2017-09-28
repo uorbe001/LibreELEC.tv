3c0b428377ff0b66a09833d8ff6c331218ab2b9d
thoradia <thoradia.quack@gmail.com>
2017-09-28 07:43:12 +0200

    pyopenssl: python build

--- packages/thoradia/depends/python/pyopenssl/package.mk
+++ packages/thoradia/depends/python/pyopenssl/package.mk
@@ -3,18 +3,9 @@ PKG_VERSION="17.3.0"
 PKG_LICENSE="ASLv2"
 PKG_SITE="https://pyopenssl.readthedocs.io/en/stable/"
 PKG_URL="https://github.com/pyca/$PKG_NAME/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain distutilscross:host Python cryptography"
+PKG_DEPENDS_TARGET="toolchain distutilscross:host Python"
 PKG_LONGDESC="A Python wrapper around the OpenSSL library"
 
-make_target() {
-  export LDSHARED="$CC -shared"
-  export PYTHONXCPREFIX="$SYSROOT_PREFIX/usr"
-  python setup.py build --cross-compile
-}
-
-makeinstall_target() {
-  python setup.py install --root=$INSTALL --prefix=/usr
-  find $INSTALL/usr/lib -name "*.py" -exec rm -rf "{}" ";"
-  rm -rf $INSTALL/usr/lib/python*/site-packages/*.egg-info \
-         $INSTALL/usr/lib/python*/site-packages/*/tests
-}
+PKG_IS_PYTHON="yes"
+PKG_PYTHON_DEPENDS_TARGET="cryptography six"
+PKG_PYTHON_OPTS_TARGET="--cross-compile"
