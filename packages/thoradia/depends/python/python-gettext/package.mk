689778ded8a955696f2d039aaf07da3e34fd1dee
thoradia <thoradia.quack@gmail.com>
2017-09-28 07:43:44 +0200

    python-gettext: python build

--- packages/thoradia/depends/python/python-gettext/package.mk
+++ packages/thoradia/depends/python/python-gettext/package.mk
@@ -6,15 +6,5 @@ PKG_URL="https://github.com/hannosch/$PKG_NAME/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain distutilscross:host Python"
 PKG_LONGDESC="Python Gettext po to mo file compiler"
 
-make_target() {
-  export LDSHARED="$CC -shared"
-  export PYTHONXCPREFIX="$SYSROOT_PREFIX/usr"
-  python setup.py build --cross-compile
-}
-
-makeinstall_target() {
-  python setup.py install --root=$INSTALL --prefix=/usr
-  find $INSTALL/usr/lib -name "*.py" -exec rm -rf "{}" ";"
-  rm -rf $INSTALL/usr/lib/python*/site-packages/*.egg-info \
-         $INSTALL/usr/lib/python*/site-packages/*/tests
-}
+PKG_IS_PYTHON="yes"
+PKG_PYTHON_OPTS_TARGET="--cross-compile"
