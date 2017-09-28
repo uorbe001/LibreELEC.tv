a05ab919f3a97468964f13b79047b367e2bae29c
thoradia <thoradia.quack@gmail.com>
2017-09-28 07:37:22 +0200

    incremental: python build

--- packages/thoradia/depends/python/incremental/package.mk
+++ packages/thoradia/depends/python/incremental/package.mk
@@ -7,15 +7,5 @@ PKG_SOURCE_DIR="incremental-$PKG_NAME-$PKG_VERSION"
 PKG_DEPENDS_TARGET="toolchain distutilscross:host Python"
 PKG_LONGDESC="A library for versioning your Python projects"
 
-make_target() {
-  export LDSHARED="$CC -shared"
-  export PYTHONXCPREFIX="$SYSROOT_PREFIX/usr"
-  python setup.py build --cross-compile
-}
-
-makeinstall_target() {
-  python setup.py install --root=$INSTALL --prefix=/usr
-  find $INSTALL/usr/lib -name "*.py" -exec rm -rf "{}" ";"
-  rm -rf $INSTALL/usr/lib/python*/site-packages/*.egg-info \
-         $INSTALL/usr/lib/python*/site-packages/*/tests
-}
+PKG_IS_PYTHON="yes"
+PKG_PYTHON_OPTS_TARGET="--cross-compile"
