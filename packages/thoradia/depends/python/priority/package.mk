61e7cfe6ae80b3b7274ca32558326ea80f65070d
thoradia <thoradia.quack@gmail.com>
2017-09-28 07:50:19 +0200

    priority: initial package

---
+++ packages/thoradia/depends/python/priority/package.mk
@@ -0,0 +1,10 @@
+PKG_NAME="priority"
+PKG_VERSION="1.3.0"
+PKG_LICENSE="MIT"
+PKG_SITE="http://python-hyper.org/priority/"
+PKG_URL="https://files.pythonhosted.org/packages/source/p/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain distutilscross:host Python"
+PKG_LONGDESC="A pure-Python implementation of the HTTP/2 priority tree"
+
+PKG_IS_PYTHON="yes"
+PKG_PYTHON_OPTS_TARGET="--cross-compile"
