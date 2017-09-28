66a33c612b8d82dfe293bd00a0c05922a922f341
thoradia <thoradia.quack@gmail.com>
2017-09-28 07:35:49 +0200

    cryptography: python build

--- packages/thoradia/depends/python/cryptography/package.mk
+++ packages/thoradia/depends/python/cryptography/package.mk
@@ -3,19 +3,13 @@ PKG_VERSION="2.0.3"
 PKG_LICENSE="BSD"
 PKG_SITE="https://cryptography.io/"
 PKG_URL="https://github.com/pyca/$PKG_NAME/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain cffi:host Python packaging"
+PKG_DEPENDS_TARGET="toolchain cffi:host Python"
 PKG_LONGDESC="A package designed to expose cryptographic primitives and recipes to Python developers"
 
-make_target() {
-  export CC="$CC -pthread"
-  export LDSHARED="$CC -shared"
-  export PYTHONXCPREFIX="$SYSROOT_PREFIX/usr"
-  python setup.py build --cross-compile
-}
+PKG_IS_PYTHON="yes"
+PKG_PYTHON_DEPENDS_TARGET="asn1crypto cffi enum34 idna ipaddress six"
+PKG_PYTHON_OPTS_TARGET="--cross-compile"
 
-makeinstall_target() {
-  python setup.py install --root=$INSTALL --prefix=/usr
-  find $INSTALL/usr/lib -name "*.pyo" -exec rm -rf "{}" ";"
-  rm -rf $INSTALL/usr/lib/python*/site-packages/*.egg-info \
-         $INSTALL/usr/lib/python*/site-packages/*/tests
+pre_configure_target() {
+  export CC="$CC -pthread"
 }
