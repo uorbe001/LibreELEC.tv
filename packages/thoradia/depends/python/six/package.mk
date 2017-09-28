9006dec5ce0f71225433fddd6a19fd189ba7357e
thoradia <thoradia.quack@gmail.com>
2017-09-28 07:45:16 +0200

    six: python build

--- packages/thoradia/depends/python/six/package.mk
+++ packages/thoradia/depends/python/six/package.mk
@@ -6,13 +6,5 @@ PKG_URL="https://github.com/benjaminp/$PKG_NAME/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain Python distutilscross:host"
 PKG_LONGDESC="Python 2 and 3 compatibility utilities"
 
-make_target() {
-  export LDSHARED="$CC -shared"
-  export PYTHONXCPREFIX="$SYSROOT_PREFIX/usr"
-  python setup.py build --cross-compile
-}
-
-makeinstall_target() {
-  python setup.py install --root=$INSTALL --prefix=/usr
-  find $INSTALL/usr/lib -name "*.py" -exec rm -rf "{}" ";"
-}
+PKG_IS_PYTHON="yes"
+PKG_PYTHON_OPTS_TARGET="--cross-compile"
