9e7129caac5327865acd9b4692c05177a7b61884
thoradia <thoradia.quack@gmail.com>
2017-09-29 12:02:33 +0200

    lxml: update to 4.0.0

--- packages/thoradia/depends/python/lxml/package.mk
+++ packages/thoradia/depends/python/lxml/package.mk
@@ -1,21 +1,11 @@
 PKG_NAME="lxml"
-PKG_VERSION="3.8.0"
+PKG_VERSION="4.0.0"
 PKG_LICENSE="BSD"
 PKG_SITE="http://lxml.de/"
-PKG_URL="https://files.pythonhosted.org/packages/source/${PKG_NAME:0:1}/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/lxml/$PKG_NAME/archive/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_SOURCE_DIR="lxml-$PKG_NAME-$PKG_VERSION"
 PKG_DEPENDS_TARGET="toolchain cython:host libxslt:host Python libxml2 libxslt"
 PKG_LONGDESC="The lxml XML toolkit for Python"
 
-make_target() {
-  export LDSHARED="$CC -shared"
-  export PYTHONXCPREFIX="$SYSROOT_PREFIX/usr"
-  python setup.py build --cross-compile
-}
-
-makeinstall_target() {
-  python setup.py install --root=$INSTALL --prefix=/usr
-  find $INSTALL/usr/lib -name "*.py" -exec rm -rf "{}" ";"
-  rm -rf $INSTALL/usr/lib/python*/site-packages/*.egg-info \
-         $INSTALL/usr/lib/python*/site-packages/*/tests
-}
+PKG_IS_PYTHON="yes"
+PKG_PYTHON_OPTS_TARGET="--cross-compile"
