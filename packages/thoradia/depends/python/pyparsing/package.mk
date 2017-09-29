6d3b6bbb225031d6448e12570f6755bfc8355050
thoradia <thoradia.quack@gmail.com>
2017-09-29 12:00:23 +0200

    pyparsing: initial package

---
+++ packages/thoradia/depends/python/pyparsing/package.mk
@@ -0,0 +1,10 @@
+PKG_NAME="pyparsing"
+PKG_VERSION="2.2.0"
+PKG_LICENSE="MIT"
+PKG_SITE="http://pyparsing.wikispaces.com/"
+PKG_URL="https://files.pythonhosted.org/packages/source/p/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain distutilscross:host Python"
+PKG_LONGDESC="Python parsing module"
+
+PKG_IS_PYTHON="yes"
+PKG_PYTHON_OPTS_TARGET="--cross-compile"
