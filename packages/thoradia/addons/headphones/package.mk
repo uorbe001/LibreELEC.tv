a7d5a1c390e2f7f0e015b08bbb15cb466a8dcbf7
thoradia <thoradia.quack@gmail.com>
2017-09-29 12:03:29 +0200

    headphones: update Python dependencies

--- packages/thoradia/addons/headphones/package.mk
+++ packages/thoradia/addons/headphones/package.mk
@@ -3,14 +3,14 @@ PKG_VERSION="0c98085"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/rembo10/headphones"
 PKG_URL="https://github.com/rembo10/$PKG_NAME/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain cffi cryptography enum34 pyopenssl"
+PKG_DEPENDS_TARGET="toolchain pyopenssl"
 PKG_SECTION="service"
 
 PKG_IS_ADDON="yes"
 PKG_ADDON_NAME="Headphones"
 PKG_ADDON_TYPE="xbmc.service"
 PKG_MAINTAINER="thoradia"
-PKG_REV="14"
+PKG_REV="15"
 PKG_SHORTDESC="$PKG_ADDON_NAME: automated music downloader for NZB and Torrent"
 PKG_LONGDESC="$PKG_ADDON_NAME ($PKG_VERSION) is an automated music downloader for NZB and Torrent. It supports SABnzbd, NZBget, Transmission, µTorrent, Deluge and Blackhole."
 PKG_DISCLAIMER="Keep it legal and carry on"
@@ -29,7 +29,7 @@ addon() {
   cp -PR $PKG_BUILD/* \
          $ADDON_BUILD/$PKG_ADDON_ID/headphones 
 
-  for d in cffi cryptography enum34 packaging pyopenssl; do
+  for d in pyopenssl; do
     cp -PR $(get_build_dir $d)/.install_pkg/usr/lib/python*/site-packages/* \
            $ADDON_BUILD/$PKG_ADDON_ID/headphones/libs
   done
