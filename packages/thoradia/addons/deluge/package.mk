609bae3597a660b1329cc1870b7c4c8825f0135f
thoradia <thoradia.quack@gmail.com>
2017-09-29 09:05:48 +0200

    deluge: simplify package.mk

--- packages/thoradia/addons/deluge/package.mk
+++ packages/thoradia/addons/deluge/package.mk
@@ -4,11 +4,11 @@ PKG_LICENSE="GPLv3"
 PKG_SITE="http://deluge-torrent.org/"
 PKG_URL="https://github.com/deluge-torrent/deluge/archive/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_SOURCE_DIR="deluge-deluge-$PKG_VERSION"
-PKG_DEPENDS_TARGET="toolchain libtorrent-rasterbar"
+PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="service"
 
 PKG_IS_PYTHON="yes"
-PKG_PYTHON_DEPENDS_TARGET="chardet mako pyopenssl python-gettext pyxdg twisted"
+PKG_PYTHON_DEPENDS_TARGET="chardet libtorrent-rasterbar mako pyopenssl python-gettext pyxdg twisted"
 
 PKG_IS_ADDON="yes"
 PKG_ADDON_NAME="Deluge"
@@ -22,14 +22,11 @@ PKG_DISCLAIMER="Keep it legal and carry on"
 addon() {
   mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/deluge
 
-  cp -PR $PKG_BUILD/.install_pkg/usr/bin/deluged \
+  cp -LR $PKG_BUILD/.install_pkg/usr/bin/deluged \
          $PKG_BUILD/.install_pkg/usr/bin/deluge-web \
          $PKG_BUILD/.install_pkg/usr/lib/python*/site-packages/* \
          $SYSROOT_PREFIX/usr/lib/libboost_python.so.* \
+         $(get_build_dir libtorrent-rasterbar)/.install_pkg/usr/lib/libtorrent-rasterbar.so.? \
          $(get_build_dir setuptools)/build/lib/pkg_resources \
          $ADDON_BUILD/$PKG_ADDON_ID/deluge/
-
-  cp -L $(get_build_dir libtorrent-rasterbar)/.install_pkg/usr/lib/libtorrent-rasterbar.so.? \
-        $(get_build_dir libtorrent-rasterbar)/.install_pkg/usr/lib/python*/site-packages/libtorrent.so \
-        $ADDON_BUILD/$PKG_ADDON_ID/deluge/
 }
