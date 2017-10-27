f3a17bc2957fad34cf7d16a2d911653f6ad92bb3
thoradia <thoradia.quack@gmail.com>
2017-10-27 15:54:10 +0200

    par2cmdline: update to 0.7.4

---
+++ packages/thoradia/depends/par2cmdline/package.mk
@@ -0,0 +1,15 @@
+PKG_NAME="par2cmdline"
+PKG_VERSION="0.7.4"
+PKG_SHA256="ea260fa804bb20514123dea06a0ad4066260cd5c76db087715468ca7e2d49c47"
+PKG_LICENSE="OSS"
+PKG_SITE="https://github.com/Parchive/par2cmdline"
+PKG_URL="https://github.com/Parchive/$PKG_NAME/archive/v$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SHORTDESC="par2cmdline: a PAR 2.0 compatible file verification and repair tool"
+PKG_AUTORECONF="yes"
+
+post_makeinstall_target() {
+  ln -fs $INSTALL/usr/bin/par2 $INSTALL/usr/bin/par2create
+  ln -fs $INSTALL/usr/bin/par2 $INSTALL/usr/bin/par2repair
+  ln -fs $INSTALL/usr/bin/par2 $INSTALL/usr/bin/par2verify
+}
