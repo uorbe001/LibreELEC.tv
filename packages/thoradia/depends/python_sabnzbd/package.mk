aa04f06d7678af89bdc1a55641d4858c7864ca0e
thoradia <thoradia.quack@gmail.com>
2017-11-02 17:30:47 +0100

    python_*: fixup

--- packages/thoradia/depends/python_sabnzbd/package.mk
+++ packages/thoradia/depends/python_sabnzbd/package.mk
@@ -6,7 +6,6 @@ PKG_LONGDESC="SABnzbd Python dependencies"
 PKG_IS_PYTHON="yes"
 
 pre_make_target() {
-  export CFLAGS="$CFLAGS -L$SYSROOT_PREFIX/usr/lib"
   export LDSHARED="-pthread"
 
   touch dummy.py
