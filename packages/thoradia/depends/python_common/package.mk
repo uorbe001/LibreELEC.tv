4bc05b41b5a2c678723d1c1d07e9bf68a4804fdf
thoradia <thoradia.quack@gmail.com>
2017-11-02 12:04:50 +0100

    python_common: initial package

---
+++ packages/thoradia/depends/python_common/package.mk
@@ -0,0 +1,41 @@
+PKG_NAME="python_common"
+PKG_VERSION="1"
+PKG_DEPENDS_TARGET="libxslt:host cffi libxml2 libxslt"
+PKG_LONGDESC="Common Python dependencies"
+
+PKG_IS_PYTHON="yes"
+
+pre_make_target() {
+  export LDSHARED="-pthread"
+
+  touch dummy.py
+
+  cat << EOF > setup.py
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='$PKG_NAME',
+      version='$PKG_VERSION',
+      description='$PKG_LONGDESC',
+      author='thoradia',
+      url='https://github.com/thoradia/LibreELEC.tv',
+      py_modules = ['dummy'],
+      install_requires=[
+          "asn1crypto==0.23.0",
+          "cryptography==2.1.2",
+          "enum34==1.1.6",
+          "idna==2.6",
+          "ipaddress==1.0.18",
+          "lxml==4.1.0",
+          "pyOpenSSL==17.3.0",
+          "setuptools==36.6.0",
+          "six==1.11.0",
+        ],
+     )
+EOF
+}
+
+post_make_target() {
+  cp -r "$(get_build_dir cffi)"/.install_pkg/lib/*.egg "$INSTALL/lib"
+}
