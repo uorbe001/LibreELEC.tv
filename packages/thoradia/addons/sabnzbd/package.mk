bbbb2e8ba0811342fa0961ccd649fac9ffa8926a
thoradia <thoradia.quack@gmail.com>
2017-11-04 16:27:29 +0100

    sabnzbd: simplify PYTHONPATH

--- packages/thoradia/addons/sabnzbd/package.mk
+++ packages/thoradia/addons/sabnzbd/package.mk
@@ -1,7 +1,7 @@
 PKG_NAME="sabnzbd"
 PKG_VERSION="2.3.1"
 PKG_SHA256="0214f8497c3422649274128dfec10090c44e8bf1911aed14cb44d877424b45ef"
-PKG_REV="25"
+PKG_REV="26"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://sabnzbd.org/"
 PKG_URL="https://github.com/sabnzbd/$PKG_NAME/archive/$PKG_VERSION.tar.gz"
@@ -36,7 +36,7 @@ addon() {
          $ADDON_BUILD/$PKG_ADDON_ID/bin/
 
   cp -PR $PKG_BUILD/* \
-         $(get_build_dir python_sabnzbd)/.install_pkg/lib/*.egg \
+         $(get_build_dir python_sabnzbd)/.install_pkg/lib/*.egg/* \
          $ADDON_BUILD/$PKG_ADDON_ID/SABnzbd/
 
   rm -fr $ADDON_BUILD/$PKG_ADDON_ID/SABnzbd/osx \
