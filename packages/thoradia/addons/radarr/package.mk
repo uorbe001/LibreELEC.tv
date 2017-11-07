665117d8ed26301499f3b3a026fdaaecd9dac0d2
thoradia <thoradia.quack@gmail.com>
2017-11-07 21:10:31 +0100

    radarr: update to 0.2.0.870

---
+++ packages/thoradia/addons/radarr/package.mk
@@ -0,0 +1,32 @@
+PKG_NAME="radarr"
+PKG_VERSION="0.2.0.870"
+PKG_SHA256="82bfd372d3b6f48ca8c254db4c4e8a4d15334b673854d4be20319fa9b1619bb7"
+PKG_REV="17"
+PKG_LICENSE="GPLv3"
+PKG_SITE="https://github.com/Radarr/Radarr"
+PKG_URL="https://github.com/Radarr/Radarr/releases/download/v$PKG_VERSION/Radarr.develop.$PKG_VERSION.linux.tar.gz"
+PKG_SOURCE_DIR="Radarr"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="service"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_NAME="Radarr"
+PKG_ADDON_TYPE="xbmc.service"
+PKG_ADDON_REQUIRES="virtual.thoradia-mono:0.0.0"
+PKG_MAINTAINER="thoradia"
+PKG_SHORTDESC="$PKG_ADDON_NAME: a fork of Sonarr to work with movies à la CouchPotato"
+PKG_LONGDESC="$PKG_ADDON_NAME ($PKG_VERSION) is a fork of Sonarr to work with movies à la CouchPotato."
+PKG_DISCLAIMER="Keep it legal and carry on"
+
+make_target() {
+  :
+}
+
+makeinstall_target() {
+  :
+}
+
+addon() {
+  mkdir -p "$ADDON_BUILD/$PKG_ADDON_ID/Radarr"
+  cp -PR "$PKG_BUILD"/* "$ADDON_BUILD/$PKG_ADDON_ID/Radarr"
+}
