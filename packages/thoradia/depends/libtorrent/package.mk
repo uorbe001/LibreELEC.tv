2750866f5abb593373ee8e2c03b4f1d7598cfdf4
thoradia <thoradia.quack@gmail.com>
2017-11-08 12:47:33 +0100

    libtorrent: rework package

---
+++ packages/thoradia/depends/libtorrent/package.mk
@@ -0,0 +1,13 @@
+PKG_NAME="libtorrent"
+PKG_VERSION="0.13.6"
+PKG_SHA256="bf963ac6e73e194a2cd87ebdf809988b5b3d6244bb7cd43d7d0c4852fc501524"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/rakshasa/libtorrent"
+PKG_URL="https://github.com/rakshasa/$PKG_NAME/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain openssl zlib"
+PKG_LONGDESC="libtorrent"
+PKG_AUTORECONF="yes"
+
+PKG_CONFIGURE_OPTS_TARGET="--enable-static \
+                           --disable-debug \
+                           --disable-shared"
