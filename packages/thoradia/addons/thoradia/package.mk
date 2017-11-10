ad0b60700b25a0b26ce34ebfa6f90c5b47af332a
thoradia <thoradia.quack@gmail.com>
2017-11-10 21:31:15 +0100

    thoradia: add aria2

--- packages/thoradia/addons/thoradia/package.mk
+++ packages/thoradia/addons/thoradia/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="thoradia"
-PKG_REV="12"
+PKG_REV="13"
 PKG_SITE="https://github.com/thoradia/thoradia"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="service"
@@ -9,7 +9,7 @@ PKG_ADDON_NAME="Thoradia Add-ons"
 PKG_ADDON_TYPE="thoradia.service"
 PKG_MAINTAINER="thoradia"
 PKG_SHORTDESC="$PKG_ADDON_NAME: add-on repository"
-PKG_LONGDESC="$PKG_ADDON_NAME ($PKG_REV) add-on repository, provides CouchPotato, Deluge, Headphones, Jackett, Medusa, NZBGet, qBittorent, Radarr, rTorrent, ruTorrent, SABnzbd, Sonarr and Transmission."
+PKG_LONGDESC="$PKG_ADDON_NAME ($PKG_REV) add-on repository, provides aria2, CouchPotato, Deluge, Headphones, Jackett, Medusa, NZBGet, qBittorent, Radarr, rTorrent, ruTorrent, SABnzbd, Sonarr and Transmission."
 PKG_DISCLAIMER="Keep it legal and carry on"
 
 make_target() {
