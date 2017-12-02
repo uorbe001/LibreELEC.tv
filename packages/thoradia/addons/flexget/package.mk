89d745b095bb8972d08f27d06db5e33d172902a8
thoradia <thoradia.quack@gmail.com>
2017-12-02 20:56:46 +0100

    flexget: update to 2.11.6

--- packages/thoradia/addons/flexget/package.mk
+++ packages/thoradia/addons/flexget/package.mk
@@ -1,7 +1,7 @@
 PKG_NAME="flexget"
-PKG_VERSION="2.11.3"
-PKG_SHA256="0c3875ddacf7e3f7e5cc35edc5ab48cf99c29dcb3dec6c8210e9a5045046d503"
-PKG_REV="2"
+PKG_VERSION="2.11.6"
+PKG_SHA256="2c51f8e3028e5f008be9375b04155e677c57dc3868da27e81a3df240608d42f2"
+PKG_REV="3"
 PKG_SITE="https://flexget.com"
 PKG_URL="https://github.com/Flexget/Flexget/archive/$PKG_VERSION.tar.gz"
 PKG_SOURCE_DIR="Flexget-$PKG_VERSION"
@@ -18,7 +18,7 @@ PKG_LONGDESC="$PKG_ADDON_NAME ($PKG_VERSION) is a program aimed to automate down
 PKG_DISCLAIMER="Keep it legal and carry on"
 
 pre_make_target() {
-  echo "setuptools==36.8.0" >> requirements.txt
+  echo "setuptools==38.2.3" >> requirements.txt
 }
 
 post_makeinstall_target() {
@@ -35,7 +35,7 @@ addon() {
 
   cp -PR "$PKG_BUILD/flexget_vanilla.py" \
          "$ADDON_BUILD/$PKG_ADDON_ID/bin"
- 
+
   cp -PR "$PKG_BUILD/.install_pkg/usr/lib" \
          "$ADDON_BUILD/$PKG_ADDON_ID"
 }
