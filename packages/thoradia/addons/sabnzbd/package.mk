6d6531fec63bd352a7df622c7973cbb34e0d6fec
thoradia <thoradia.quack@gmail.com>
2017-12-03 13:42:40 +0100

    sabnzbd: rework

--- packages/thoradia/addons/sabnzbd/package.mk
+++ packages/thoradia/addons/sabnzbd/package.mk
@@ -1,7 +1,7 @@
 PKG_NAME="sabnzbd"
 PKG_VERSION="2.3.1"
 PKG_SHA256="0214f8497c3422649274128dfec10090c44e8bf1911aed14cb44d877424b45ef"
-PKG_REV="26"
+PKG_REV="27"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://sabnzbd.org/"
 PKG_URL="https://github.com/sabnzbd/$PKG_NAME/archive/$PKG_VERSION.tar.gz"
@@ -21,20 +21,22 @@ PKG_TOOLCHAIN="python2"
 
 make_target() {
   rm -fr linux osx six win
-  "$TOOLCHAIN/bin/python" "$TOOLCHAIN/lib/python2.7/compileall.py" -q .
+  "$TOOLCHAIN/bin/python" "$TOOLCHAIN/lib/$PKG_PYTHON_VERSION/compileall.py" -q .
   find . -name "*.py" -exec rm -rf "{}" ";"
 }
 
 addon() {
   mkdir -p "$ADDON_BUILD/$PKG_ADDON_ID/bin" \
-           "$ADDON_BUILD/$PKG_ADDON_ID/SABnzbd"
+           "$ADDON_BUILD/$PKG_ADDON_ID/sabnzbd"
 
   cp -PR "$(get_build_dir p7zip)"/bin/* \
          "$(get_build_dir par2cmdline)/.install_pkg/usr/bin/par2" \
          "$(get_build_dir unrar)/unrar" \
          "$ADDON_BUILD/$PKG_ADDON_ID/bin"
 
+  cp -PR "$(get_build_dir python_sabnzbd)/.install_pkg/usr/lib" \
+         "$ADDON_BUILD/$PKG_ADDON_ID"
+
   cp -PR "$PKG_BUILD"/* \
-         "$(get_build_dir python_sabnzbd)/.install_pkg/usr/lib/$PKG_PYTHON_VERSION/site-packages"/*.egg \
-         "$ADDON_BUILD/$PKG_ADDON_ID/SABnzbd"
+         "$ADDON_BUILD/$PKG_ADDON_ID/sabnzbd"
 }
