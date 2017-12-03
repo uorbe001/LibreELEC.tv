2e4f18b066cd8e5f9700d6b1b9f502418ec0c9d7
thoradia <thoradia.quack@gmail.com>
2017-12-03 15:26:00 +0100

    headphones: rework

--- packages/thoradia/addons/headphones/package.mk
+++ packages/thoradia/addons/headphones/package.mk
@@ -1,7 +1,7 @@
 PKG_NAME="headphones"
 PKG_VERSION="da9287d"
 PKG_SHA256="78108b833681c2df0c94272d2e8754e0f31daa3a1d4b6006306421f30bed4f7a"
-PKG_REV="18"
+PKG_REV="19"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/rembo10/headphones"
 PKG_URL="https://github.com/rembo10/$PKG_NAME/archive/$PKG_VERSION.tar.gz"
@@ -16,13 +16,7 @@ PKG_SHORTDESC="$PKG_ADDON_NAME: automated music downloader for NZB and Torrent"
 PKG_LONGDESC="$PKG_ADDON_NAME ($PKG_VERSION) is an automated music downloader for NZB and Torrent. It supports SABnzbd, NZBget, Transmission, µTorrent, Deluge and Blackhole."
 PKG_DISCLAIMER="Keep it legal and carry on"
 
-make_target() {
-  :
-}
-
-makeinstall_target() {
-  :
-}
+PKG_TOOLCHAIN="manual"
 
 addon() {
   mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/headphones
@@ -30,6 +24,6 @@ addon() {
   cp -PR $PKG_BUILD/* \
          $ADDON_BUILD/$PKG_ADDON_ID/headphones 
 
-  cp -PR $(get_build_dir python_common)/.install_pkg/lib/*.egg/* \
-         $ADDON_BUILD/$PKG_ADDON_ID/headphones/lib
+  cp -PR "$(get_build_dir python_common)/.install_pkg/usr/lib" \
+         "$ADDON_BUILD/$PKG_ADDON_ID"
 }
