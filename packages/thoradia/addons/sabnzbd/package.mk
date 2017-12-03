b7c87efad45b16cd10bb7ee51ab5be0411abc317
thoradia <thoradia.quack@gmail.com>
2017-12-03 11:43:33 +0100

    sabnzbd: backport

--- packages/thoradia/addons/sabnzbd/package.mk
+++ packages/thoradia/addons/sabnzbd/package.mk
@@ -1,7 +1,7 @@
 PKG_NAME="sabnzbd"
 PKG_VERSION="2.3.1"
 PKG_SHA256="0214f8497c3422649274128dfec10090c44e8bf1911aed14cb44d877424b45ef"
-PKG_REV="26"
+PKG_REV="27"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://sabnzbd.org/"
 PKG_URL="https://github.com/sabnzbd/$PKG_NAME/archive/$PKG_VERSION.tar.gz"
@@ -17,28 +17,26 @@ PKG_SHORTDESC="$PKG_ADDON_NAME: free and easy binary newsreader"
 PKG_LONGDESC="$PKG_ADDON_NAME ($PKG_VERSION) is a program to download binary files from Usenet servers."
 PKG_DISCLAIMER="Keep it legal and carry on"
 
-make_target() {
-  : # nop
-}
+PKG_TOOLCHAIN="python2"
 
-makeinstall_target() {
-  $ROOT/$TOOLCHAIN/bin/python $ROOT/$TOOLCHAIN/lib/python2.7/compileall.py -q .
+make_target() {
+  rm -fr linux osx six win
+  "$ROOT/$TOOLCHAIN/bin/python" "$ROOT/$TOOLCHAIN/lib/python2.7/compileall.py" -q .
   find . -name "*.py" -exec rm -rf "{}" ";"
 }
 
 addon() {
-  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin \
-           $ADDON_BUILD/$PKG_ADDON_ID/SABnzbd
+  mkdir -p "$ADDON_BUILD/$PKG_ADDON_ID/bin" \
+           "$ADDON_BUILD/$PKG_ADDON_ID/sabnzbd"
 
-  cp -PR $(get_build_dir p7zip)/bin/* \
-         $(get_build_dir par2cmdline)/.install_pkg/usr/bin/par2 \
-         $(get_build_dir unrar)/unrar \
-         $ADDON_BUILD/$PKG_ADDON_ID/bin/
+  cp -PR "$(get_build_dir p7zip)"/bin/* \
+         "$(get_build_dir par2cmdline)/.install_pkg/usr/bin/par2" \
+         "$(get_build_dir unrar)/unrar" \
+         "$ADDON_BUILD/$PKG_ADDON_ID/bin"
 
-  cp -PR $PKG_BUILD/* \
-         $(get_build_dir python_sabnzbd)/.install_pkg/lib/*.egg/* \
-         $ADDON_BUILD/$PKG_ADDON_ID/SABnzbd/
+  cp -PR "$(get_build_dir python_sabnzbd)/.install_pkg/usr/lib" \
+         "$ADDON_BUILD/$PKG_ADDON_ID"
 
-  rm -fr $ADDON_BUILD/$PKG_ADDON_ID/SABnzbd/osx \
-         $ADDON_BUILD/$PKG_ADDON_ID/SABnzbd/win
+  cp -PR "$PKG_BUILD"/* \
+         "$ADDON_BUILD/$PKG_ADDON_ID/sabnzbd"
 }
