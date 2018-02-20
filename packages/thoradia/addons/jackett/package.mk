079adcdbe47ffb4e923c31c36483548f58022015
thoradia <thoradia.quack@gmail.com>
2018-02-20 18:59:38 +0100

    jackett: set PKG_TOOLCHAIN

--- packages/thoradia/addons/jackett/package.mk
+++ packages/thoradia/addons/jackett/package.mk
@@ -18,13 +18,7 @@ PKG_SHORTDESC="$PKG_ADDON_NAME: proxy to tracker sites"
 PKG_LONGDESC="$PKG_ADDON_NAME ($PKG_VERSION) works as a proxy server: it translates queries from applications (CouchPotato, Mylar, SickRage, Sonarr,  etc.) into tracker-site-specific HTTP queries, parses the HTML response, then sends results back to the requesting software. This allows for getting recent uploads (like RSS) and performing searches. Jackett is a single repository of maintained indexer scraping and translation logic, removing the burden from other applications."
 PKG_DISCLAIMER="Keep it legal and carry on"
 
-make_target() {
-  : # nop
-}
-
-makeinstall_target() {
-  : # nop
-}
+PKG_TOOLCHAIN="manual"
 
 addon() {
   mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/Jackett
