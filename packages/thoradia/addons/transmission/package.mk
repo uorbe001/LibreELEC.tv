709c930b5735d3b4b2b2ba278fa9d9aebc44add3
thoradia <thoradia.quack@gmail.com>
2018-05-05 00:35:32 +0200

    transmission: update to 2.94

--- packages/thoradia/addons/transmission/package.mk
+++ packages/thoradia/addons/transmission/package.mk
@@ -1,12 +1,13 @@
 PKG_NAME="transmission"
-PKG_VERSION="2.93"
-PKG_SHA256="8815920e0a4499bcdadbbe89a4115092dab42ce5199f71ff9a926cfd12b9b90b"
-PKG_REV="15"
+PKG_VERSION="2.94"
+PKG_SHA256="35442cc849f91f8df982c3d0d479d650c6ca19310a994eccdaa79a4af3916b7d"
+PKG_REV="16"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.transmissionbt.com/"
 PKG_URL="https://github.com/transmission/transmission-releases/raw/master/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain curl libevent"
 PKG_SECTION="service"
+PKG_TOOLCHAIN="cmake-make"
 
 PKG_IS_ADDON="yes"
 PKG_ADDON_NAME="Transmission"
@@ -16,25 +17,21 @@ PKG_SHORTDESC="$PKG_ADDON_NAME: a fast, easy and free BitTorrent client"
 PKG_LONGDESC="$PKG_ADDON_NAME ($PKG_VERSION) is a fast, easy and free BitTorrent client."
 PKG_DISCLAIMER="Keep it legal and carry on"
 
-PKG_TOOLCHAIN="autotools"
-PKG_CONFIGURE_OPTS_TARGET="--enable-static  \
-                           --enable-daemon  \
-                           --enable-utp     \
-                           --disable-nls    \
-                           --disable-shared \
-                           --with-gnu-ld"
-
-makeinstall_target() {
-  :
-}
+PKG_CMAKE_OPTS_TARGET="-DENABLE_DAEMON=ON \
+                       -DENABLE_GTK=OFF \
+                       -DENABLE_QT=OFF \
+                       -DENABLE_UTILS=OFF \
+                       -DENABLE_CLI=OFF \
+                       -DENABLE_TESTS=OFF \
+                       -DENABLE_LIGHTWEIGHT=OFF \
+                       -DENABLE_UTP=ON \
+                       -DENABLE_NLS=OFF \
+                       -DINSTALL_DOC=OFF \
+                       -DINSTALL_LIB=OFF"
 
 addon() {
-  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
-  cp "$PKG_BUILD/.$TARGET_NAME/daemon/transmission-daemon" \
-     "$PKG_BUILD/.$TARGET_NAME/daemon/transmission-remote" \
-     "$ADDON_BUILD/$PKG_ADDON_ID/bin"
-
-  cp -PR "$PKG_BUILD/web" "$ADDON_BUILD/$PKG_ADDON_ID"
-  rm -fr "$ADDON_BUILD/$PKG_ADDON_ID/web/LICENSE"
-  find "$ADDON_BUILD/$PKG_ADDON_ID/web" -name "Makefile*" -delete
+  mkdir -p "$ADDON_BUILD/$PKG_ADDON_ID"
+  cp -PR "$PKG_BUILD/.install_pkg/usr/bin" \
+         "$PKG_BUILD/.install_pkg/usr/share/transmission/web" \
+         "$ADDON_BUILD/$PKG_ADDON_ID"
 }
