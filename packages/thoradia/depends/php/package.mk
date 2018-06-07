e2a92a01f16e5920e8aacbab892c52c4255cd2b5
thoradia <thoradia.quack@gmail.com>
2018-06-07 17:08:51 +0200

    php: update to 7.2.5

--- packages/thoradia/depends/php/package.mk
+++ packages/thoradia/depends/php/package.mk
@@ -1,6 +1,6 @@
 PKG_NAME="php"
-PKG_VERSION="7.1.11"
-PKG_SHA256="074093e9d7d21afedc5106904218a80a47b854abe368d2728ed22184c884893e"
+PKG_VERSION="7.2.5"
+PKG_SHA256="af70a33b3f7a51510467199b39af151333fbbe4cc21923bad9c7cf64268cddb2"
 PKG_LICENSE="PHP"
 PKG_SITE="http://www.php.net"
 PKG_URL="http://www.php.net/distributions/$PKG_NAME-$PKG_VERSION.tar.xz"
@@ -19,7 +19,6 @@ PKG_CONFIGURE_OPTS_TARGET="--disable-all \
                            --with-zlib=$SYSROOT_PREFIX/usr"
 
 pre_configure_target() {
-  strip_lto
   export CFLAGS="$CFLAGS -pthread"
 }
 
