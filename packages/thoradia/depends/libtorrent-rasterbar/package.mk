edd9e7d3368f4266912f2de770ca069d780ab6d4
thoradia <thoradia.quack@gmail.com>
2018-08-16 20:21:54 +0200

    libtorrent-rasterbar: update to 1.1.9

--- packages/thoradia/depends/libtorrent-rasterbar/package.mk
+++ packages/thoradia/depends/libtorrent-rasterbar/package.mk
@@ -1,6 +1,6 @@
 PKG_NAME="libtorrent-rasterbar"
-PKG_VERSION="1_1_8"
-PKG_SHA256="fdb66d8d785c6533d2da83879db062834266a44663841dd1b58dfd25280216de"
+PKG_VERSION="1_1_9"
+PKG_SHA256="29ac52c04412702cb99e38113e37ef9663e18045925f26388628204fcabbc9de"
 PKG_LICENSE="https://github.com/arvidn/libtorrent/blob/master/LICENSE"
 PKG_SITE="http://libtorrent.org/"
 PKG_URL="https://github.com/arvidn/libtorrent/archive/libtorrent-$PKG_VERSION.tar.gz"
