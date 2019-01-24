16f709d5438058d969e5b851c35a84424c7723bb
thoradia <22841905+thoradia@users.noreply.github.com>
2019-01-24 23:45:50 +0100

    radarr: start after kodi and mono

--- packages/thoradia/addons/radarr/package.mk
+++ packages/thoradia/addons/radarr/package.mk
@@ -1,7 +1,7 @@
 PKG_NAME="radarr"
 PKG_VERSION="0.2.0.1217"
 PKG_SHA256="5e0ef4d5c1fc3c6c1f113f9be0565c1c0b82e51d011890543a0b957db1dbff27"
-PKG_REV="20"
+PKG_REV="21"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/Radarr/Radarr"
 PKG_URL="https://github.com/Radarr/Radarr/releases/download/v$PKG_VERSION/Radarr.v$PKG_VERSION.linux.tar.gz"
@@ -20,6 +20,6 @@ PKG_DISCLAIMER="Keep it legal and carry on"
 PKG_TOOLCHAIN="manual"
 
 addon() {
-  mkdir -p "$ADDON_BUILD/$PKG_ADDON_ID/Radarr"
-  cp -PR "$PKG_BUILD"/* "$ADDON_BUILD/$PKG_ADDON_ID/Radarr"
+  mkdir -p "$ADDON_BUILD/$PKG_ADDON_ID/radarr"
+  cp -PR "$PKG_BUILD"/* "$ADDON_BUILD/$PKG_ADDON_ID/radarr"
 }
