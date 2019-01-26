09cbb7c1e49985819ef4188973f995b9b9c6e14e
thoradia <22841905+thoradia@users.noreply.github.com>
2019-01-26 09:15:16 +0100

    thoradia: mention new add-ons

--- packages/thoradia/addons/thoradia/package.mk
+++ packages/thoradia/addons/thoradia/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="thoradia"
-PKG_REV="19"
+PKG_REV="20"
 PKG_SITE="https://github.com/thoradia/thoradia"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="service"
@@ -9,11 +9,11 @@ PKG_ADDON_NAME="Thoradia Add-ons"
 PKG_ADDON_TYPE="thoradia.service"
 PKG_MAINTAINER="thoradia"
 PKG_SHORTDESC="$PKG_ADDON_NAME: add-on repository"
-PKG_LONGDESC="$PKG_ADDON_NAME ($PKG_REV) add-on repository, provides aria2, btfs, CouchPotato, Deluge, FlexGet, Headphones, Jackett, Lidarr, Medusa, NZBGet, NZBHydra, qBittorent, Radarr, rTorrent, ruTorrent, SABnzbd, Sonarr and Transmission."
+PKG_LONGDESC="$PKG_ADDON_NAME ($PKG_REV) add-on repository, provides aria2, Bazarr, btfs, CouchPotato, Deluge, FlexGet, Headphones, Jackett, Lidarr, Medusa, Mono, NZBGet, NZBHydra, qBittorent, Radarr, rTorrent, ruTorrent, SABnzbd, Sonarr, Thoradia VPN interface, Transmission and WebGrab+Plus."
 PKG_DISCLAIMER="Keep it legal and carry on"
 
 PKG_TOOLCHAIN="manual"
 
 addon() {
-  mkdir -p "$ADDON_BUILD/$PKG_ADDON_ID"
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID
 }
