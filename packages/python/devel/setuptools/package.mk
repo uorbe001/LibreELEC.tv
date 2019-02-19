609d68b06a58b71bd797efe8ba973ee86e1c51ef
thoradia <22841905+thoradia@users.noreply.github.com>
2019-02-19 17:35:59 +0100

    setuptools: downgrade python2 setuptools

--- packages/python/devel/setuptools/package.mk
+++ packages/python/devel/setuptools/package.mk
@@ -1,23 +1,4 @@
-# SPDX-License-Identifier: GPL-2.0-or-later
-# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
-# Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
-
 PKG_NAME="setuptools"
-PKG_VERSION="32.1.2"
-PKG_SHA256="7589316802e7716e5250cd0a188bb4b77ea9338aa275dc01500732ee25140c3b"
-PKG_LICENSE="OSS"
-PKG_SITE="https://pypi.org/project/setuptools"
-PKG_URL="https://github.com/pypa/setuptools/archive/v$PKG_VERSION.tar.gz"
-PKG_DEPENDS_HOST="Python2:host Python3:host"
-PKG_LONGDESC="Replaces Setuptools as the standard method for working with Python module distributions."
-PKG_TOOLCHAIN="manual"
-
-make_host() {
-  python2 bootstrap.py
-  python3 bootstrap.py
-}
-
-makeinstall_host() {
-  exec_thread_safe python2 setup.py install --prefix=$TOOLCHAIN
-  exec_thread_safe python3 setup.py install --prefix=$TOOLCHAIN
-}
+PKG_DEPENDS_HOST="setuptools2:host setuptools3:host"
+PKG_DEPENDS_TARGET="setuptools2 setuptools3"
+PKG_SECTION="virtual"
