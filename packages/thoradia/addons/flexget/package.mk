27d8025b810990ec4ec80245b9c0a1e02a917dce
thoradia <22841905+thoradia@users.noreply.github.com>
2019-03-26 20:57:34 +0100

    flexget: add deluge-client

--- packages/thoradia/addons/flexget/package.mk
+++ packages/thoradia/addons/flexget/package.mk
@@ -1,7 +1,7 @@
 PKG_NAME="flexget"
 PKG_VERSION="2.17.22"
 PKG_SHA256="affa6fa36ca6cb001b48fa09e076c16b64d6f31b51d7be49834ce47fd7a82445"
-PKG_REV="9"
+PKG_REV="10"
 PKG_SITE="https://flexget.com"
 PKG_URL="https://github.com/Flexget/Flexget/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="libyaml"
@@ -17,10 +17,11 @@ PKG_LONGDESC="$PKG_ADDON_NAME ($PKG_VERSION) is a program aimed to automate down
 PKG_DISCLAIMER="Keep it legal and carry on"
 
 pre_make_target() {
-  echo "mechanize==0.3.7" >> requirements.txt
-  echo "setuptools==40.6.3" >> requirements.txt
-  echo "transmissionrpc==0.11" >> requirements.txt
+  echo "deluge-client"==1.7.0 >> requirements.txt
+  echo "mechanize==0.4.1" >> requirements.txt
   echo "python-telegram-bot"==11.1.0 >> requirements.txt
+  echo "setuptools==40.8.0" >> requirements.txt
+  echo "transmissionrpc==0.11" >> requirements.txt
 }
 
 post_makeinstall_target() {
