fd4d53425815c891bc8ee1d2f9b9d3df3bbe7c73
thoradia <22841905+thoradia@users.noreply.github.com>
2019-05-10 09:06:20 +0200

    flexget: fix package

--- packages/thoradia/addons/flexget/package.mk
+++ packages/thoradia/addons/flexget/package.mk
@@ -1,7 +1,7 @@
 PKG_NAME="flexget"
 PKG_VERSION="2.17.22"
 PKG_SHA256="affa6fa36ca6cb001b48fa09e076c16b64d6f31b51d7be49834ce47fd7a82445"
-PKG_REV="10"
+PKG_REV="11"
 PKG_SITE="https://flexget.com"
 PKG_URL="https://github.com/Flexget/Flexget/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="libyaml"
@@ -16,10 +16,10 @@ PKG_SHORTDESC="$PKG_ADDON_NAME: automate downloading or processing content from 
 PKG_LONGDESC="$PKG_ADDON_NAME ($PKG_VERSION) is a program aimed to automate downloading or processing content (torrents, podcasts, etc.) from different sources like RSS-feeds, html-pages, various sites and more."
 PKG_DISCLAIMER="Keep it legal and carry on"
 
-pre_make_target() {
-  echo "deluge-client"==1.7.0 >> requirements.txt
+pre_configure_target() {
+  echo "deluge-client==1.7.0" >> requirements.txt
   echo "mechanize==0.4.1" >> requirements.txt
-  echo "python-telegram-bot"==11.1.0 >> requirements.txt
+  echo "python-telegram-bot==11.1.0" >> requirements.txt
   echo "setuptools==40.8.0" >> requirements.txt
   echo "transmissionrpc==0.11" >> requirements.txt
 }
