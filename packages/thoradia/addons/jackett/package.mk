9829e387dca43575ee8fa78f61063b86b6a97099
thoradia <22841905+thoradia@users.noreply.github.com>
2019-05-25 19:13:24 +0200

    Revert "jackett: update to 0.11.361 standalone"
    This reverts commit a6349543b1f9c46c3ffc2e4d895698a4aa5c3ac9.

--- packages/thoradia/addons/jackett/package.mk
+++ packages/thoradia/addons/jackett/package.mk
@@ -1,26 +1,17 @@
 PKG_NAME="jackett"
-PKG_VERSION="0.11.361"
-PKG_REV="39"
+PKG_VERSION="0.11.295"
+PKG_SHA256="dff371d1e2933b67effcf34c55e6fe8cfb6e994108b08c0bf3970cafd07f1597"
+PKG_REV="38"
 PKG_LICENSE="GPL2"
 PKG_SITE="https://github.com/Jackett/Jackett"
-PKG_DEPENDS_TARGET="toolchain curl curl3 krb5 lttng-ust"
+PKG_URL="https://github.com/Jackett/Jackett/releases/download/v$PKG_VERSION/Jackett.Binaries.Mono.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="service"
-
-case "$ARCH" in
-  "arm")
-    PKG_SHA256="9eb9eb10e0b8c528a987fff9b5bfded28e00240b8364df448baed41940cb902f"
-    PKG_URL="https://github.com/Jackett/Jackett/releases/download/v$PKG_VERSION/Jackett.Binaries.LinuxARM32.tar.gz"
-    ;;
-  "x86_64")
-    PKG_SHA256="0d612e28bc45923883a17c166f3842e85895ea84dfb7a42b1855fdf9749c79e1"
-    PKG_URL="https://github.com/Jackett/Jackett/releases/download/v$PKG_VERSION/Jackett.Binaries.LinuxAMDx64.tar.gz"
-    ;;
-esac
 
 PKG_IS_ADDON="yes"
 PKG_ADDON_NAME="Jackett"
-PKG_ADDON_PROJECTS="any !RPi1"
 PKG_ADDON_TYPE="xbmc.service"
+PKG_ADDON_REQUIRES="tools.mono:0.0.0"
 PKG_MAINTAINER="thoradia"
 PKG_SHORTDESC="$PKG_ADDON_NAME: proxy to tracker sites"
 PKG_LONGDESC="$PKG_ADDON_NAME ($PKG_VERSION) works as a proxy server: it translates queries from applications (CouchPotato, Mylar, SickRage, Sonarr,  etc.) into tracker-site-specific HTTP queries, parses the HTML response, then sends results back to the requesting software. This allows for getting recent uploads (like RSS) and performing searches. Jackett is a single repository of maintained indexer scraping and translation logic, removing the burden from other applications."
@@ -31,15 +22,4 @@ PKG_TOOLCHAIN="manual"
 addon() {
   mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/jackett
   cp -PR $PKG_BUILD/* $ADDON_BUILD/$PKG_ADDON_ID/jackett
-
-  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/libs
-  cp -L $(get_build_dir curl3)/.install_pkg/usr/lib/libcurl.so.? \
-        $(get_build_dir krb5)/.install_pkg/usr/lib/libcom_err.so.? \
-        $(get_build_dir krb5)/.install_pkg/usr/lib/libgssapi_krb5.so.? \
-        $(get_build_dir krb5)/.install_pkg/usr/lib/libk5crypto.so.? \
-        $(get_build_dir krb5)/.install_pkg/usr/lib/libkrb5.so.? \
-        $(get_build_dir krb5)/.install_pkg/usr/lib/libkrb5support.so.? \
-        $(get_build_dir lttng-ust)/.install_pkg/usr/lib/liblttng-ust.so.? \
-        $(get_build_dir lttng-ust)/.install_pkg/usr/lib/liblttng-ust-tracepoint.so.? \
-        $ADDON_BUILD/$PKG_ADDON_ID/libs
 }
