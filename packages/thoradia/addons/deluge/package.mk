3ba53eb908211b93d60e3ddef5006da7b0fdc01f
thoradia <22841905+thoradia@users.noreply.github.com>
2019-06-09 01:01:32 +0200

    deluge: update to 2.0.2

--- packages/thoradia/addons/deluge/package.mk
+++ packages/thoradia/addons/deluge/package.mk
@@ -1,7 +1,7 @@
 PKG_NAME="deluge"
-PKG_VERSION="1.3.15"
-PKG_SHA256="6f2accf55bd97828f81ea13d5f29087103eb4f95b6be957323e23174cdc86826"
-PKG_REV="26"
+PKG_VERSION="2.0.2"
+PKG_SHA256="1af0be38c597d5194c9c531b12cd4560638de24c9fa127c2aa91778dbb60530a"
+PKG_REV="27"
 PKG_LICENSE="GPLv3"
 PKG_SITE="http://deluge-torrent.org/"
 PKG_URL="https://github.com/deluge-torrent/deluge/archive/$PKG_NAME-$PKG_VERSION.tar.gz"
@@ -19,24 +19,28 @@ PKG_DISCLAIMER="Keep it legal and carry on"
 PKG_TOOLCHAIN="python2"
 
 pre_configure_target() {
+  echo $PKG_VERSION > RELEASE-VERSION
   export LDSHARED="-pthread"
   python setup.py build
 }
 
 post_make_target() {
-  _site="usr/lib/$PKG_PYTHON_VERSION/site-packages"
-  cp -r "$(get_build_dir cffi)/.install_pkg/$_site"/cffi-*.egg \
-         "$INSTALL/$_site"
-  cat "$(get_build_dir cffi)/.install_pkg/$_site/easy-install.pth" >> "$INSTALL/$_site/easy-install.pth"
+  _site=usr/lib/$PKG_PYTHON_VERSION/site-packages
+  cp -r $(get_build_dir cffi)/.install_pkg/$_site/cffi-*.egg \
+         $INSTALL/$_site
+  cat $(get_build_dir cffi)/.install_pkg/$_site/easy-install.pth >> $INSTALL/$_site/easy-install.pth
 }
 
 addon() {
-  mkdir -p "$ADDON_BUILD/$PKG_ADDON_ID/bin"
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
+    for f in "deluged" "deluge-web"; do
+      cp -L $PKG_BUILD/.install_pkg/usr/bin/$f $ADDON_BUILD/$PKG_ADDON_ID/bin
+      sed -i "1c #!/usr/bin/env python2" $ADDON_BUILD/$PKG_ADDON_ID/bin/$f
+    done
 
-  cp -PR "$PKG_BUILD/.install_pkg/usr/lib" \
-         "$ADDON_BUILD/$PKG_ADDON_ID"
-
-  cp -LR "$(get_build_dir libtorrent-rasterbar)/.install_pkg/usr/lib"/libtorrent-rasterbar.so.? \
-         "$(get_build_dir libtorrent-rasterbar)/.install_pkg/usr/lib/$PKG_PYTHON_VERSION" \
-         "$ADDON_BUILD/$PKG_ADDON_ID/lib"
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/lib
+    cp -LR $PKG_BUILD/.install_pkg/usr/lib/* \
+           $(get_build_dir libtorrent-rasterbar)/.install_pkg/usr/lib/libtorrent-rasterbar.so.?? \
+           $(get_build_dir libtorrent-rasterbar)/.install_pkg/usr/lib/python* \
+           $ADDON_BUILD/$PKG_ADDON_ID/lib
 }
