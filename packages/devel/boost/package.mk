7e2af297efc3ccb60df800a3fc7b1e6469b1afe6
thoradia <22841905+thoradia@users.noreply.github.com>
2019-06-18 19:51:45 +0200

    boost: update to 1.66.0

--- packages/devel/boost/package.mk
+++ packages/devel/boost/package.mk
@@ -2,11 +2,12 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="boost"
-PKG_VERSION="1_65_1"
-PKG_SHA256="9807a5d16566c57fd74fb522764e0b134a8bbe6b6e8967b83afefd30dcd3be81"
+PKG_VERSION="1_66_0"
+PKG_SHA256="5721818253e6a0989583192f96782c4a98eb6204965316df9f5ad75819225ca9"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.boost.org/"
-PKG_URL="$SOURCEFORGE_SRC/boost/boost/1.65.1/${PKG_NAME}_${PKG_VERSION}.tar.bz2"
+PKG_URL="$SOURCEFORGE_SRC/boost/boost/1.66.0/${PKG_NAME}_${PKG_VERSION}.tar.bz2"
+PKG_SOURCE_DIR="${PKG_NAME}_${PKG_VERSION}"
 PKG_DEPENDS_TARGET="toolchain boost:host Python2 zlib bzip2"
 PKG_LONGDESC="boost: Peer-reviewed STL style libraries for C++"
 PKG_TOOLCHAIN="manual"
