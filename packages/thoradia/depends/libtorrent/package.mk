f0d611345c573f014498bb833030a4c34b94e38a
thoradia <22841905+thoradia@users.noreply.github.com>
2019-08-04 09:13:41 +0200

    libtorrent: update to 0.13.8

--- packages/thoradia/depends/libtorrent/package.mk
+++ packages/thoradia/depends/libtorrent/package.mk
@@ -1,6 +1,6 @@
 PKG_NAME="libtorrent"
-PKG_VERSION="0.13.7"
-PKG_SHA256="86b4b1753385aaddf9e59ad94f1292eee5102139eb57520e84d1af2f04693708"
+PKG_VERSION="0.13.8"
+PKG_SHA256="0f6c2e7ffd3a1723ab47fdac785ec40f85c0a5b5a42c1d002272205b988be722"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/rakshasa/libtorrent"
 PKG_URL="https://github.com/rakshasa/$PKG_NAME/archive/v$PKG_VERSION.tar.gz"
