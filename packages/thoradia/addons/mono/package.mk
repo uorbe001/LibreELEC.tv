609146565d5f5807732cfc5ed22e3df72bc7d6b5
thoradia <22841905+thoradia@users.noreply.github.com>
2019-09-30 07:15:31 +0200

    mono: update to 6.4.0.198, fix system.d

--- packages/thoradia/addons/mono/package.mk
+++ packages/thoradia/addons/mono/package.mk
@@ -2,17 +2,17 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mono"
-PKG_VERSION="6.0.0.319"
-PKG_SHA256="6b4c80b88bf7688178797bfecc681e3dbc4c6810413c97d6f3c476061a8d261a"
-PKG_REV="114"
+PKG_VERSION="6.4.0.198"
+PKG_SHA256="d00852822525e36f9f8b3e0f537d3a41c7a718cac22d06fc63ea64988877c2ea"
+PKG_REV="115"
 PKG_ARCH="any"
 PKG_LICENSE="MIT"
 PKG_SITE="http://www.mono-project.com"
 PKG_URL="http://download.mono-project.com/sources/mono/$PKG_NAME-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain mono:host libgdiplus sqlite zlib"
+PKG_DEPENDS_TARGET="toolchain mono:host sqlite zlib"
 PKG_SECTION="tools"
 PKG_SHORTDESC="Mono: a cross platform, open source .NET framework"
-PKG_LONGDESC="Mono ($PKG_VERSION) is a software platform designed to allow developers to easily create cross platform applications part of the .NET Foundation"
+PKG_LONGDESC="Mono ($PKG_VERSION) is a software platform designed to allow developers to easily create cross platform applications part of the .NET Foundation."
 PKG_TOOLCHAIN="autotools"
 
 PKG_IS_ADDON="yes"
@@ -29,9 +29,8 @@ options="--build=$HOST_NAME \
          --libexecdir=$prefix/lib \
          --localstatedir=/var \
          --disable-boehm \
-         --disable-libraries \
-         --without-mcs-docs \
-         --without-x"
+         --with-shared-mono=no \
+         --without-mcs-docs"
 
 configure_host() {
   ../configure $options --host=$HOST_NAME
@@ -43,8 +42,7 @@ makeinstall_host() {
 
 configure_target() {
   ../configure $options --host=$TARGET_NAME \
-                        --disable-mcs-build \
-                        --with-libgdiplus=$SYSROOT_PREFIX/usr/lib
+                        --disable-mcs-build
 }
 
 makeinstall_target() {
