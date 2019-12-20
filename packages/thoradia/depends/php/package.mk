f5ef7abc640132c1dac5ec5d3ba026cf01b6182a
thoradia <22841905+thoradia@users.noreply.github.com>
2019-12-20 12:13:34 +0100

    php: update to 7.3.13, enable mbstring

--- packages/thoradia/depends/php/package.mk
+++ packages/thoradia/depends/php/package.mk
@@ -1,9 +1,9 @@
 PKG_NAME="php"
-PKG_VERSION="7.3.8"
-PKG_SHA256="f6046b2ae625d8c04310bda0737ac660dc5563a8e04e8a46c1ee24ea414ad5a5"
+PKG_VERSION="7.3.13"
+PKG_SHA256="57ac55fe442d2da650abeb9e6fa161bd3a98ba6528c029f076f8bba43dd5c228"
 PKG_LICENSE="PHP"
 PKG_SITE="http://www.php.net"
-PKG_URL="http://www.php.net/distributions/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_URL="http://www.php.net/distributions/${PKG_NAME}-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain curl pcre"
 PKG_LONGDESC="The PHP Interpreter"
 PKG_AUTORECONF="no"
@@ -13,16 +13,17 @@ PKG_CONFIGURE_OPTS_TARGET="--disable-all \
                            --disable-phpdbg \
                            --enable-cgi \
                            --enable-json \
-                           --with-curl=$SYSROOT_PREFIX/usr \
-                           --with-openssl=$SYSROOT_PREFIX/usr \
+                           --enable-mbstring \
+                           --with-curl=${SYSROOT_PREFIX}/usr \
+                           --with-openssl=${SYSROOT_PREFIX}/usr \
                            --with-pcre-regex \
-                           --with-zlib=$SYSROOT_PREFIX/usr"
+                           --with-zlib=${SYSROOT_PREFIX}/usr"
 
 pre_configure_target() {
-  export CFLAGS="$CFLAGS -pthread"
+  export CFLAGS="${CFLAGS} -pthread"
 }
 
 makeinstall_target() {
-  mkdir -p "$INSTALL/usr/bin"
-  cp "sapi/cgi/php-cgi" "$INSTALL/usr/bin/php"
+  mkdir -p ${INSTALL}/usr/bin
+    cp sapi/cgi/php-cgi ${INSTALL}/usr/bin/php
 }
