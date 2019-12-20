6a756972d3c9fb456350a5620bd1613cef0351d3
thoradia <22841905+thoradia@users.noreply.github.com>
2019-12-20 08:40:38 +0100

    libevent: tidy package

--- packages/thoradia/depends/libevent/package.mk
+++ packages/thoradia/depends/libevent/package.mk
@@ -3,8 +3,8 @@ PKG_VERSION="2.1.11-stable"
 PKG_SHA256="229393ab2bf0dc94694f21836846b424f3532585bac3468738b7bf752c03901e"
 PKG_LICENSE="BSD"
 PKG_SITE="http://libevent.org/"
-PKG_URL="https://github.com/libevent/$PKG_NAME/archive/release-$PKG_VERSION.tar.gz"
-PKG_SOURCE_DIR="$PKG_NAME-release-$PKG_VERSION"
+PKG_URL="https://github.com/libevent/${PKG_NAME}/archive/release-${PKG_VERSION}.tar.gz"
+PKG_SOURCE_DIR="${PKG_NAME}-release-${PKG_VERSION}"
 PKG_DEPENDS_TARGET="toolchain openssl zlib"
 PKG_SHORTDESC="libevent: a library for asynchronous event notification"
 PKG_TOOLCHAIN="autotools"
@@ -14,7 +14,3 @@ PKG_CONFIGURE_OPTS_TARGET="--disable-libevent-regress \
                            --disable-shared \
                            --enable-static \
                            --enable-openssl"
-
-post_makeinstall_target() {
-  rm -fr "$INSTALL"
-}
