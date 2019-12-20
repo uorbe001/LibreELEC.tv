34b00c6b591ccc90b1c6983a2280ec092c717608
thoradia <22841905+thoradia@users.noreply.github.com>
2019-12-20 01:26:55 +0100

    libtorrent-rasterbar: update to 1.2.3

--- packages/thoradia/depends/libtorrent-rasterbar/package.mk
+++ packages/thoradia/depends/libtorrent-rasterbar/package.mk
@@ -1,6 +1,6 @@
 PKG_NAME="libtorrent-rasterbar"
-PKG_VERSION="1_2_2"
-PKG_SHA256="353793a234858e8dae9ee4a37d59c8c5297b718cc7ae3efe5798f004c0da6856"
+PKG_VERSION="1_2_3"
+PKG_SHA256="6381020213c5138adf8bcf9f5bf04ab1521d563cd8a9fa1156f1590b35ccc9d4"
 PKG_LICENSE="https://github.com/arvidn/libtorrent/blob/master/LICENSE"
 PKG_SITE="http://libtorrent.org/"
 PKG_URL="https://github.com/arvidn/libtorrent/archive/libtorrent-$PKG_VERSION.tar.gz"
@@ -9,14 +9,10 @@ PKG_LONGDESC="An efficient feature complete C++ bittorrent implementation"
 PKG_TOOLCHAIN="autotools"
 
 PKG_CONFIGURE_OPTS_TARGET="--enable-python-binding \
-                           --with-boost-libdir=$SYSROOT_PREFIX/usr/lib \
+                           --with-boost-libdir=${SYSROOT_PREFIX}/usr/lib \
                            --with-libiconv"
 
 post_unpack() {
-  mkdir -p $PKG_BUILD/build-aux/
-  touch $PKG_BUILD/build-aux/config.rpath
-}
-
-pre_configure_target() {
-  export CXXFLAGS="$CXXFLAGS -lboost_python27 -lpython2.7"
+  mkdir -p ${PKG_BUILD}/build-aux/
+    touch ${PKG_BUILD}/build-aux/config.rpath
 }
