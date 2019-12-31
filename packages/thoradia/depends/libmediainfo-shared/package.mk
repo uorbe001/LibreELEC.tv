bee648fdeea67f38c84aa6bda2b5745a7e37b23d
thoradia <22841905+thoradia@users.noreply.github.com>
2019-12-31 15:43:19 +0100

    libmediainfo-shared: version 20.03

---
+++ packages/thoradia/depends/libmediainfo-shared/package.mk
@@ -0,0 +1,26 @@
+PKG_NAME="libmediainfo-shared"
+PKG_VERSION="20.03"
+PKG_SHA256="2a1da41b94aa5380c537f198f3c797fd9ef23891709e935143e4535099437311"
+PKG_LICENSE="GPL"
+PKG_SITE="http://mediaarea.net/en/MediaInfo/Download/Source"
+PKG_URL="https://github.com/MediaArea/MediaInfoLib/archive/v${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain libzen zlib"
+PKG_DEPENDS_CONFIG="libzen"
+PKG_LONGDESC="MediaInfo is a convenient unified display of the most relevant technical and tag data for video and audio files"
+PKG_TOOLCHAIN="manual"
+
+configure_target() {
+  cd Project/GNU/Library
+    do_autoreconf
+    ./configure \
+          --host=${TARGET_NAME} \
+          --build=${HOST_NAME} \
+          --disable-static \
+          --enable-shared \
+          --prefix=/usr \
+          --enable-visibility
+}
+
+make_target() {
+    make
+}
