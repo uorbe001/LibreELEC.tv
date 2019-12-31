448a8ddfe61c093568df4ef76c375f858879ff11
thoradia <22841905+thoradia@users.noreply.github.com>
2019-12-31 15:42:43 +0100

    thoradia: version 24, split add-on versions in corresponding git branches

---
+++ packages/thoradia/addons/thoradia/package.mk
@@ -0,0 +1,19 @@
+PKG_NAME="thoradia"
+PKG_VERSION="0"
+PKG_REV="24"
+PKG_SITE="https://github.com/thoradia/thoradia"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_TOOLCHAIN="manual"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_NAME="Thoradia Add-ons"
+PKG_ADDON_TYPE="thoradia.service"
+PKG_SECTION="service"
+PKG_SHORTDESC="${PKG_ADDON_NAME}: add-on repository"
+PKG_LONGDESC="${PKG_ADDON_NAME} (${PKG_REV}) add-on repository, provides aria2, Bazarr, btfs, CouchPotato, Deluge, FlexGet, Headphones, Jackett, Lidarr, Medusa, Mono, NZBGet, NZBHydra, qBittorent, Radarr, rTorrent, ruTorrent, SABnzbd, Sonarr, Thoradia VPN interface, Transmission and WebGrab+Plus."
+PKG_MAINTAINER="thoradia"
+PKG_DISCLAIMER="Keep it legal and carry on"
+
+addon() {
+  :
+}
