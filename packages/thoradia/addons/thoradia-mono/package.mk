29496a0d6fd0b690baad52e49da4ec0272aeeefa
thoradia <22841905+thoradia@users.noreply.github.com>
2020-04-23 17:07:08 +0200

    thoradia-mono: libmediainfo-shared version 20.03

--- packages/thoradia/addons/thoradia-mono/package.mk
+++ packages/thoradia/addons/thoradia-mono/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="thoradia-mono"
-PKG_REV="11"
+PKG_REV="12"
 PKG_DEPENDS_TARGET="toolchain libmediainfo-shared"
 
 PKG_IS_ADDON="yes"
@@ -8,12 +8,12 @@ PKG_ADDON_TYPE="xbmc.python.script"
 PKG_ADDON_REQUIRES="tools.mono:0.0.0"
 PKG_SECTION="virtual"
 PKG_SHORTDESC="${PKG_ADDON_NAME}: a bundle of tools required by Mono add-ons"
-PKG_LONGDESC="${PKG_ADDON_NAME} (${PKG_REV}) includes libmediainfo shared library."
+PKG_LONGDESC="${PKG_ADDON_NAME} (${PKG_REV}) includes the libmediainfo shared library."
 PKG_MAINTAINER="thoradia"
 PKG_DISCLAIMER="Keep it legal and carry on"
 
 addon() {
   mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}/lib
-    cp -L $(get_build_dir libmediainfo-shared)/.install_pkg/usr/lib/libmediainfo.so.0 \
+    cp -L $(get_build_dir libmediainfo-shared)/Project/GNU/Library/.libs/libmediainfo.so.0 \
           ${ADDON_BUILD}/${PKG_ADDON_ID}/lib/
 }
