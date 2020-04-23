4922ee5685dd04819755a1e62597a169f8f33b31
thoradia <22841905+thoradia@users.noreply.github.com>
2020-04-23 17:33:46 +0200

    sonarr: version 3.0.3.770

--- packages/thoradia/addons/sonarr/package.mk
+++ packages/thoradia/addons/sonarr/package.mk
@@ -1,25 +1,25 @@
 PKG_NAME="sonarr"
-PKG_VERSION="3.0.3.644"
-PKG_SHA256="e2baed5e3ec1c1804813a173313101c9fabb4921da09aa42ca0197ff371246d4"
-PKG_REV="34"
+PKG_VERSION="3.0.3.770"
+PKG_SHA256="84e40e07bcc7b152e5ac65cee639bb7fd6e578587cf957b5d50df1ee829531ea"
+PKG_REV="35"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://sonarr.tv/"
-PKG_URL="http://download.sonarr.tv/v3/phantom-develop/$PKG_VERSION/Sonarr.phantom-develop.$PKG_VERSION.linux.tar.gz"
+PKG_URL="http://download.sonarr.tv/v3/phantom-develop/${PKG_VERSION}/Sonarr.phantom-develop.${PKG_VERSION}.linux.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
-PKG_SECTION="service"
+PKG_TOOLCHAIN="manual"
 
 PKG_IS_ADDON="yes"
 PKG_ADDON_NAME="Sonarr"
 PKG_ADDON_TYPE="xbmc.service"
 PKG_ADDON_REQUIRES="virtual.thoradia-mono:0.0.0"
+PKG_SECTION="service"
+PKG_SHORTDESC="${PKG_ADDON_NAME}: a PVR for Usenet and BitTorrent users"
+PKG_LONGDESC="${PKG_ADDON_NAME} (${PKG_VERSION}) is a PVR for Usenet and BitTorrent users. It can monitor multiple RSS feeds for new episodes of your favorite shows and will grab, sort and rename them. It can also be configured to automatically upgrade the quality of files already downloaded when a better quality format becomes available."
 PKG_MAINTAINER="thoradia"
-PKG_SHORTDESC="$PKG_ADDON_NAME: a PVR for Usenet and BitTorrent users"
-PKG_LONGDESC="$PKG_ADDON_NAME ($PKG_VERSION) is a PVR for Usenet and BitTorrent users. It can monitor multiple RSS feeds for new episodes of your favorite shows and will grab, sort and rename them. It can also be configured to automatically upgrade the quality of files already downloaded when a better quality format becomes available."
 PKG_DISCLAIMER="Keep it legal and carry on"
 
-PKG_TOOLCHAIN="manual"
-
 addon() {
-  mkdir -p "$ADDON_BUILD/$PKG_ADDON_ID/sonarr"
-  cp -PR $PKG_BUILD/* $ADDON_BUILD/$PKG_ADDON_ID/sonarr
+  mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}/libs
+    cp -PR ${PKG_BUILD}/* \
+           ${ADDON_BUILD}/${PKG_ADDON_ID}/libs
 }
