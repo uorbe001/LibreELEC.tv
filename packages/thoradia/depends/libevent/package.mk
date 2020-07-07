1c00636f15d6e6aafab5182b6c90bfe2474c4c20
thoradia <22841905+thoradia@users.noreply.github.com>
2020-07-07 09:30:23 +0200

    libevent: update to 2.1.12-stable

--- packages/thoradia/depends/libevent/package.mk
+++ packages/thoradia/depends/libevent/package.mk
@@ -1,16 +1,15 @@
 PKG_NAME="libevent"
-PKG_VERSION="2.1.11-stable"
-PKG_SHA256="229393ab2bf0dc94694f21836846b424f3532585bac3468738b7bf752c03901e"
+PKG_VERSION="2.1.12-stable"
+PKG_SHA256="7180a979aaa7000e1264da484f712d403fcf7679b1e9212c4e3d09f5c93efc24"
 PKG_LICENSE="BSD"
 PKG_SITE="http://libevent.org/"
 PKG_URL="https://github.com/libevent/${PKG_NAME}/archive/release-${PKG_VERSION}.tar.gz"
-PKG_SOURCE_DIR="${PKG_NAME}-release-${PKG_VERSION}"
 PKG_DEPENDS_TARGET="toolchain openssl zlib"
-PKG_SHORTDESC="libevent: a library for asynchronous event notification"
-PKG_TOOLCHAIN="autotools"
+PKG_LONGDESC="libevent: a library for asynchronous event notification"
 
-PKG_CONFIGURE_OPTS_TARGET="--disable-libevent-regress \
-                           --disable-samples \
-                           --disable-shared \
-                           --enable-static \
-                           --enable-openssl"
+PKG_CMAKE_OPTS_TARGET="-DEVENT__DISABLE_BENCHMARK=ON \
+                       -DEVENT__DISABLE_DEBUG_MODE=ON \
+                       -DEVENT__DISABLE_REGRESS=ON \
+                       -DEVENT__DISABLE_SAMPLES=ON \
+                       -DEVENT__DISABLE_TESTS=ON \
+                       -DEVENT__LIBRARY_TYPE=STATIC"
