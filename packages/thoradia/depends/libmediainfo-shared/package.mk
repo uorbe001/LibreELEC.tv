9c576c649ae779ecfe959b95755ab7dc74694715
thoradia <22841905+thoradia@users.noreply.github.com>
2020-07-28 13:37:44 +0200

    libmediainfo-shared: version 20.03

--- packages/thoradia/depends/libmediainfo-shared/package.mk
+++ packages/thoradia/depends/libmediainfo-shared/package.mk
@@ -1,27 +1,26 @@
 PKG_NAME="libmediainfo-shared"
-PKG_VERSION="18.12"
-PKG_SHA256="e8f88fe9ab1ded093bd1c1e057e5633b5768479b6cd1327b9174361f78d77ca7"
+PKG_VERSION="20.03"
+PKG_SHA256="2a1da41b94aa5380c537f198f3c797fd9ef23891709e935143e4535099437311"
 PKG_LICENSE="GPL"
 PKG_SITE="http://mediaarea.net/en/MediaInfo/Download/Source"
-PKG_URL="https://github.com/MediaArea/MediaInfoLib/archive/v$PKG_VERSION.tar.gz"
-PKG_SOURCE_DIR="MediaInfoLib-$PKG_VERSION"
+PKG_URL="https://github.com/MediaArea/MediaInfoLib/archive/v${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain libzen zlib"
-PKG_SHORTDESC="MediaInfo is a convenient unified display of the most relevant technical and tag data for video and audio files"
-PKG_TOOLCHAIN="make"
+PKG_LONGDESC="MediaInfo is a convenient unified display of the most relevant technical and tag data for video and audio files"
+PKG_TOOLCHAIN="manual"
+
+configure_target() {
+  CXXFLAGS="${CXXFLAGS} -lpthread -lzen"
+  cd Project/GNU/Library
+    do_autoreconf
+    ./configure \
+          --host=${TARGET_NAME} \
+          --build=${HOST_NAME} \
+          --disable-static \
+          --enable-shared \
+          --prefix=/usr \
+          --enable-visibility
+}
 
 make_target() {
-  CXXFLAGS="$CXXFLAGS -lpthread -lzen"
-  cd Project/GNU/Library
-  do_autoreconf
-  ./configure \
-        --host=$TARGET_NAME \
-        --build=$HOST_NAME \
-        --disable-static \
-        --enable-shared \
-        --prefix=/usr \
-        --enable-visibility \
-        --with-gnu-ld \
-        --without-libcurl \
-        --without-libmms
-  make
+    make
 }
