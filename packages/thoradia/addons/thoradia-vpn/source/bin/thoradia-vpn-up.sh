6ae2dbae1cb5dc0ac951f40b1cd127d5fd7b0391
thoradia <22841905+thoradia@users.noreply.github.com>
2020-07-28 13:40:18 +0200

    thoradia-vpn: version 2

--- packages/thoradia/addons/thoradia-vpn/source/bin/thoradia-vpn-up.sh
+++ packages/thoradia/addons/thoradia-vpn/source/bin/thoradia-vpn-up.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-ip addr flush dev "$dev"
-ip addr add "$ifconfig_local/$ifconfig_netmask" dev "$dev"
+ip addr flush dev "${dev}"
+ip addr add "${ifconfig_local}/${ifconfig_netmask}" dev "${dev}"
 
-ip route flush dev "$dev"
-ip route add default dev "$dev" src "$ifconfig_local" table 1
+ip route flush dev "${dev}"
+ip route add default dev "${dev}" src "${ifconfig_local}" table 1
 
-ip rule add from "$ifconfig_local" table 1
-ip rule add to "$ifconfig_local" table 1
+ip rule add from "${ifconfig_local}" table 1
+ip rule add to "${ifconfig_local}" table 1
 
 echo "Rules:"
 ip rule list
