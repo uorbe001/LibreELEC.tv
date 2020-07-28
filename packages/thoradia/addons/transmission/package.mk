8591f1905facead55b5a4673bb7cc6fff5d194d1
thoradia <22841905+thoradia@users.noreply.github.com>
2020-07-28 13:15:21 +0200

    transmission: version 3.00

--- packages/thoradia/addons/transmission/package.mk
+++ packages/thoradia/addons/transmission/package.mk
@@ -1,20 +1,20 @@
 PKG_NAME="transmission"
-PKG_VERSION="3.00-beta"
-PKG_SHA256="6c5a30c737bcab51a93f99cae4e1e5d3a1115934aa63effefc8a8cea1dfae320"
-PKG_REV="18"
+PKG_VERSION="3.00"
+PKG_SHA256="9144652fe742f7f7dd6657716e378da60b751aaeda8bef8344b3eefc4db255f2"
+PKG_REV="23"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.transmissionbt.com/"
-PKG_URL="https://raw.githubusercontent.com/thoradia/LibreELEC.tv/sources/transmission-3.00-beta.tar.gz"
-PKG_DEPENDS_TARGET="toolchain curl libiconv libevent systemd"
-PKG_SECTION="service"
+PKG_URL="https://github.com/transmission/transmission-releases/raw/master/${PKG_NAME}-${PKG_VERSION}.tar.xz"
+PKG_DEPENDS_TARGET="toolchain curl libevent"
 PKG_TOOLCHAIN="cmake-make"
 
 PKG_IS_ADDON="yes"
 PKG_ADDON_NAME="Transmission"
 PKG_ADDON_TYPE="xbmc.service"
+PKG_SECTION="service"
+PKG_SHORTDESC="${PKG_ADDON_NAME}: a fast, easy and free BitTorrent client"
+PKG_LONGDESC="${PKG_ADDON_NAME} (${PKG_VERSION}) is a fast, easy and free BitTorrent client."
 PKG_MAINTAINER="thoradia"
-PKG_SHORTDESC="$PKG_ADDON_NAME: a fast, easy and free BitTorrent client"
-PKG_LONGDESC="$PKG_ADDON_NAME ($PKG_VERSION) is a fast, easy and free BitTorrent client."
 PKG_DISCLAIMER="Keep it legal and carry on"
 
 PKG_CMAKE_OPTS_TARGET="-DENABLE_DAEMON=ON \
@@ -22,7 +22,7 @@ PKG_CMAKE_OPTS_TARGET="-DENABLE_DAEMON=ON \
                        -DENABLE_QT=OFF \
                        -DENABLE_MAC=OFF \
                        -DENABLE_UTILS=OFF \
-                       -DENABLE_CLI=OFF \
+                       -DENABLE_CLI=ON \
                        -DENABLE_TESTS=OFF \
                        -DENABLE_LIGHTWEIGHT=OFF \
                        -DENABLE_UTP=ON \
@@ -36,11 +36,11 @@ PKG_CMAKE_OPTS_TARGET="-DENABLE_DAEMON=ON \
                        -DUSE_SYSTEM_UTP=OFF \
                        -DUSE_SYSTEM_B64=OFF \
                        -DWITH_CRYPTO=openssl \
-                       -DWITH_SYSTEMD=ON"
+                       -DWITH_SYSTEMD=OFF"
 
 addon() {
-  mkdir -p "$ADDON_BUILD/$PKG_ADDON_ID"
-  cp -PR "$PKG_BUILD/.install_pkg/usr/bin" \
-         "$PKG_BUILD/.install_pkg/usr/share/transmission/web" \
-         "$ADDON_BUILD/$PKG_ADDON_ID"
+  mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}
+    cp -PR ${PKG_BUILD}/.install_pkg/usr/bin \
+           ${PKG_BUILD}/.install_pkg/usr/share/transmission/web \
+           ${ADDON_BUILD}/${PKG_ADDON_ID}
 }
